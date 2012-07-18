b04e8b0b57986f2aba99f23a79359396839e9809
FHorse <punes.development@gmail.com>
2012-07-18 11:18:16 +0200

    Ripristino filtro audio none.

--- src/filters/audio/none.c
+++ src/filters/audio/none.c
@@ -91,12 +91,17 @@ SWORD audio_filter_apu_mixer_none(void) {
 /* --------------------------------------------------------------------------------------- */
 /*                                   Extra Audio Mixer                                     */
 /* --------------------------------------------------------------------------------------- */
-WORD audio_filters_none_FDS(SWORD mixer) {
-	/*return (mixer + (fds.snd.main.output + (fds.snd.main.output >> 1)));*/
-	return (mixer + fds.snd.main.output);
+SWORD audio_filters_none_FDS(SWORD mixer) {
+	mixer_cut_and_high();
+
+	return (mixer + (fds.snd.main.output + (fds.snd.main.output >> 1)));
 }
 SWORD audio_filters_none_MMC5(SWORD mixer) {
-	return (mixer + (mmc5.S3.output + mmc5.S4.output + mmc5.pcmSample));
+	mixer += (mmc5.S3.output + mmc5.S4.output) + mmc5.pcmSample;
+
+	mixer_cut_and_high();
+
+	return (mixer);
 }
 SWORD audio_filters_none_Namco_N163(SWORD mixer) {
 	BYTE i;
@@ -104,31 +109,32 @@ SWORD audio_filters_none_Namco_N163(SWORD mixer) {
 
 	for (i = n163.sndChStart; i < 8; i++) {
 		if (n163.ch[i].active) {
-			a += ((n163.ch[i].output << 1) * (n163.ch[i].volume >> 2));
+			a += (n163.ch[i].output * (n163.ch[i].volume >> 2));
 		}
 	}
 
 	mixer += a;
 
-	apuMixerCutAndHigh();
+	mixer_cut_and_high();
 
 	return (mixer);
 }
 SWORD audio_filters_none_Sunsoft_FM7(SWORD mixer) {
-	mixer += ((fm7.square[0].output << 1) + (fm7.square[1].output << 1)
-	        + (fm7.square[2].output << 1));
+	mixer += (fm7.square[0].output + fm7.square[1].output + fm7.square[2].output);
 
-	apuMixerCutAndHigh();
+	mixer_cut_and_high();
 
 	return (mixer);
 }
 SWORD audio_filters_none_VRC6(SWORD mixer) {
-	mixer += ((vrc6.S3.output << 1) + (vrc6.S4.output << 1)) + (vrc6.saw.output / 5);
+	mixer += (vrc6.S3.output + vrc6.S4.output) + vrc6.saw.output;
 
-	apuMixerCutAndHigh();
+	mixer_cut_and_high();
 
 	return (mixer);
 }
 SWORD audio_filters_none_VRC7(SWORD mixer) {
+	mixer_cut_and_high();
+
 	return (mixer + (opll_calc() << 2));
 }
