5f9f7d2cc20a7aaecd0d19dd5d4ab01c3953c56f
FHorse <punes.development@gmail.com>
2012-08-04 18:53:42 +0200

    inizio versione 0.66.

--- src/version.h
+++ src/version.h
@@ -9,8 +9,8 @@
 #define VERSION_H_
 
 #define VER1        "0"
-#define VER2        "65"
-#define VERSION     VER1 "." VER2
+#define VER2        "66"
+#define VERSION     VER1 "." VER2 " WIP"
 #define NAME        "puNES"
 #define AUTHOR      "FHorse"
 #define COPYRIGTH   "(C) " AUTHOR " 2012"
