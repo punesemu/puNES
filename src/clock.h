fd0d2d99ff1f8557cc8b32d87642c309ded80a61
FHorse <punes.development@gmail.com>
2012-08-13 21:11:52 +0200

    correzioni typo.

--- src/clock.h
+++ src/clock.h
@@ -11,21 +11,21 @@
 typedef struct {
 	WORD type;             /* il tipo di formato */
 	BYTE fps;              /* il framerate */
-	double baseHz;          /* il clock di base (Hz) */
+	double baseHz;         /* il clock di base (Hz) */
 	BYTE baseDivide;       /* il divisore del clock di base */
-	double masterHz;        /* master clock (Hz) = ((baseClock / baseClockDivide) * 6) */
+	double masterHz;       /* master clock (Hz) = ((baseClock / baseClockDivide) * 6) */
 	WORD masterHB;         /* numero di master cycles necessari per una scanline */
 	WORD totalLines;       /* numero totale delle linee disegnate */
 	WORD visibleLines;     /* numero delle scanlines visibili */
 	BYTE vintLines;        /* durata (in scanlines) del VINT */
-	double ppuHz;           /* PPU clock (masterHz / ppuDivide) */
+	double ppuHz;          /* PPU clock (masterHz / ppuDivide) */
 	BYTE ppuDivide;        /* il divisore del master clock per ottenere il PPU clock */
 	BYTE ppuFor1CPU;       /* numero di cicli PPU che servono per avere un ciclo CPU */
 	SDBWORD ppuCylesPermitWrite; /* i cicli CPU dall'avvio/reset prima di
                                   * poter scrivere nei registri della PPU */
 	BYTE ppuOpenbusFrames; /* frames per il decadimento di un bit openbus del PPU */
 	BYTE cpuDivide;        /* CPU clock e' 12 volte piu' lento del master clock */
-	double cpuHz;           /* CPU clock (masterHz / cpuDivide) */
+	double cpuHz;          /* CPU clock (masterHz / cpuDivide) */
 	float cpuCyclesFrame;  /* numero di cicli CPU per ogni frame (CPU Clock / fps) */
 	double msFrame;        /* i ms dedicati ad ogni frame */
 } _machine;
