df55f0a619986b9415381901303346387b35158f
FHorse <punes.development@gmail.com>
2014-02-28 01:49:06 +0100

    aggiunta la versione a 64bit della D3D9.

---
+++ D3D9_win64/src/filters/audio/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/filters/audio/blip.c \
+../src/filters/audio/blip2.c \
+../src/filters/audio/original.c 
+
+OBJS += \
+./src/filters/audio/blip.o \
+./src/filters/audio/blip2.o \
+./src/filters/audio/original.o 
+
+C_DEPS += \
+./src/filters/audio/blip.d \
+./src/filters/audio/blip2.d \
+./src/filters/audio/original.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/filters/audio/%.o: ../src/filters/audio/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross GCC Compiler'
+	x86_64-w64-mingw32-gcc -DMINGW64 -DD3D9 -I../src -I../src/d3d9 -I/home/fhorse/sviluppo/personale/punes/misc/DXSDK/Include -I/home/fhorse/sviluppo/personale/punes/misc/DXSDK/vc10/include -O3 -Wall -ffast-math -c -fmessage-length=0 -finline-functions -Winline -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
