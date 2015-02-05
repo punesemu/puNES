0b9b8cce2b7af38c30251f60405f3a035e061f14
fhorse <punes.development@gmail.com>
2015-02-05 11:40:51 +0100

    un if e' piu' che sufficiente

--- src/gui/pStyle.cpp
+++ src/gui/pStyle.cpp
@@ -11,11 +11,9 @@ pStyle::pStyle() : QProxyStyle() {}
 pStyle::~pStyle() {}
 int pStyle::styleHint(StyleHint hint, const QStyleOption* opt = 0, const QWidget* widget = 0,
         QStyleHintReturn* returnData = 0) const {
-	switch(hint) {
-		case QStyle::SH_Menu_SloppySubMenus:
-			return (0);
-		default:
-			return (QProxyStyle::styleHint(hint, opt, widget, returnData));
+	if (hint == QStyle::SH_Menu_SloppySubMenus) {
+		return (0);
 	}
+	return (QProxyStyle::styleHint(hint, opt, widget, returnData));
 }
 
