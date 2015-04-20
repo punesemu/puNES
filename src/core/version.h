f0511a41685b3759f73012d688bf5fa2efddf6bc
fhorse <punes.development@gmail.com>
2015-04-20 12:49:24 +0200

    corretto il link per la donazione

--- src/core/version.h
+++ src/core/version.h
@@ -21,10 +21,9 @@
 #define VERSION     VER1 "." VER2 " WIP"
 #define NAME        "puNES"
 #define AUTHOR      "FHorse"
-#define COMPILED    "04202015111149"
+#define COMPILED    "04202015124755"
 #define WEBSITE     "http://forums.nesdev.com/viewtopic.php?f=3&amp;t=6928"
-#define DONATE      "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=UN6ZAETM7HHTC"
-
+#define DONATE      "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=QPPXNRL5NAHDC"
 
 #define COPYRIGTH   " 2015 by " AUTHOR
 #define COPYRANSI   "(C)" COPYRIGTH
