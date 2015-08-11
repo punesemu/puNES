8e471ad8d0a4f2d75d2531b4d6a3c928659b258e
fhorse <punes.development@gmail.com>
2015-08-11 09:35:30 +0200

    aggiornata la versione a 0.95

--- src/core/version.h
+++ src/core/version.h
@@ -15,13 +15,13 @@
 #define VER1        "0"
 #define VER1_INT    0
 
-#define VER2        "94"
-#define VER2_INT    94
+#define VER2        "95"
+#define VER2_INT    95
 
 #define VERSION     VER1 "." VER2 " WIP"
 #define NAME        "puNES"
 #define AUTHOR      "FHorse"
-#define COMPILED    "04232015014703"
+#define COMPILED    "08082015121729"
 #define WEBSITE     "http://forums.nesdev.com/viewtopic.php?f=3&amp;t=6928"
 #define DONATE      "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=QPPXNRL5NAHDC"
 
