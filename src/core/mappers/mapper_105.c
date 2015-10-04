457f7d47f4f6a06eb1ad97d2620dd1e0f7eb0026
fhorse <punes.development@gmail.com>
2015-10-04 15:20:47 +0200

    modificata l'intestazione con riferirimento al copyright e alla licenza.

--- src/core/mappers/mapper_105.c
+++ src/core/mappers/mapper_105.c
@@ -1,10 +1,22 @@
 /*
- * mapper_105.c
+ *  Copyright (C) 2010-2016 Fabio Cavallo (aka FHorse)
  *
- *  Created on: 08/gen/2014
- *      Author: fhorse
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
- *  Note : Questa mapper e' una MMC1 modificata.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+/*
+ * questa e' una mappe MMC1 modificata.
  */
 
 #include <string.h>
