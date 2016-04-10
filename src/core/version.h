40b326a5c3527af12706fa976f41dbb51e9c483a
fhorse <punes.development@gmail.com>
2016-04-10 17:15:54 +0200

    Corretto aggiornamento data e orario di compilazione.

--- src/core/version.h
+++ src/core/version.h
@@ -19,10 +19,6 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-//#if !defined (RELEASE)
-//#include "build.h"
-//#endif
-
 #define VER1        "0"
 #define VER1_INT    0
 
@@ -37,11 +33,10 @@
 
 #define NAME        "puNES"
 #define AUTHOR      "FHorse"
-#define COMPILED    "10092015191317"
 #define WEBSITE     "http://forums.nesdev.com/viewtopic.php?f=3&amp;t=6928"
 #define DONATE      "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=QPPXNRL5NAHDC"
 
-#define COPYRIGTH   " 2015 by " AUTHOR
+#define COPYRIGTH   " 2016 by " AUTHOR
 #define COPYRANSI   "(C)" COPYRIGTH
 #define COPYRUTF8   "&#169;" COPYRIGTH
 
