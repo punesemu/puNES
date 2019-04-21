188b9dbc0e6a1ec24d0949677f1dc5a43406eea8
Fabio Cavallo (FHorse) <punes.development@gmail.com>
2019-04-22 00:21:02 +0200

    New translations en_EN.ts (Spanish)

--- src/gui/designer/translations/es_ES.ts
+++ src/gui/designer/translations/es_ES.ts
@@ -832,7 +832,7 @@
     </message>
     <message>
       <source>A description must be entered</source>
-      <translation type="unfinished">A description must be entered</translation>
+      <translation>Debe ingresar una descripción</translation>
     </message>
   </context>
   <context>
