punesemu/puNES

commit 008455ebf8af4af9dd62f43ff4fed888e3e44049

Fixed the vertical stretch in fullscreen mode.

src/video/d3d9/d3d9.c
@@ -28,13 +28,13 @@
 #include "overscan.h"
 
 #define _SCR_ROWS_BRD\
-	((float)  (SCR_ROWS - (overscan.borders->left + overscan.borders->right)) * gfx.pixel_aspect_ratio)
+	((float)(SCR_ROWS - (overscan.borders->left + overscan.borders->right)) * gfx.pixel_aspect_ratio)
 #define _SCR_LINES_BRD\
-	(float) (SCR_LINES - (overscan.borders->up + overscan.borders->down))
+	(float)(SCR_LINES - (overscan.borders->up + overscan.borders->down))
 #define _SCR_ROWS_NOBRD\
-	((float) SCR_ROWS * gfx.pixel_aspect_ratio)
+	((float)SCR_ROWS * gfx.pixel_aspect_ratio)
 #define _SCR_LINES_NOBRD\
-	(float) SCR_LINES
+	(float)SCR_LINES
 
 static void d3d9_shader_cg_error_handler(void);
 static BYTE d3d9_device_create(UINT width, UINT height);
@@ -278,22 +278,14 @@ BYTE d3d9_context_create(void) {
 		// configuro l'aspect ratio del fullscreen
 		if (cfg->fullscreen) {
 			if (!cfg->stretch) {
-				float ratio_frame = (float)gfx.w[VIDEO_MODE] / (float)gfx.h[VIDEO_MODE];
-				float ratio_surface;
+				int mul = gfx.w[VIDEO_MODE] > gfx.h[VIDEO_MODE] ?
+					(gfx.h[VIDEO_MODE] - (gfx.h[VIDEO_MODE] % gfx.h[PASS0])) / gfx.h[PASS0] :
+					(gfx.w[VIDEO_MODE] - (gfx.w[VIDEO_MODE] % gfx.w[PASS0])) / gfx.w[PASS0];
 
-				if (overscan.enabled && (cfg->oscan_black_borders_fscr == FALSE)) {
-					ratio_surface = _SCR_ROWS_BRD / _SCR_LINES_BRD;
-				} else {
-					ratio_surface = _SCR_ROWS_NOBRD / _SCR_LINES_NOBRD;
-				}
-
-				if (ratio_frame > ratio_surface) {
-					vp->w = (int)((float)gfx.h[VIDEO_MODE] * ratio_surface);
-					vp->x = (int)(((float)gfx.w[VIDEO_MODE] - (float)vp->w) * 0.5f);
-				} else {
-					vp->h = (int)((float)gfx.w[VIDEO_MODE] / ratio_surface);
-					vp->y = (int)(((float)gfx.h[VIDEO_MODE] - (float)vp->h) * 0.5f);
-				}
+				vp->w = gfx.w[PASS0] * mul;
+				vp->h = gfx.h[PASS0] * mul;
+				vp->x = (gfx.w[VIDEO_MODE] - vp->w) >> 1;
+				vp->y = (gfx.h[VIDEO_MODE] - vp->h) >> 1;
 			}
 
 			if (overscan.enabled && (cfg->oscan_black_borders_fscr == FALSE)) {
@@ -405,12 +397,12 @@ BYTE d3d9_context_create(void) {
 			prev = &d3d9.texture[i - 1].rect;
 		}
 
-		shd->info.video_size.x = (FLOAT) prev->base.w;
-		shd->info.video_size.y = (FLOAT) prev->base.h;
+		shd->info.video_size.x = (FLOAT)prev->base.w;
+		shd->info.video_size.y = (FLOAT)prev->base.h;
 		shd->info.texture_size.x = prev->w,
 		shd->info.texture_size.y = prev->h;
-		shd->info.output_size.x = (FLOAT) texture->vp.w;
-		shd->info.output_size.y = (FLOAT) texture->vp.h;
+		shd->info.output_size.x = (FLOAT)texture->vp.w;
+		shd->info.output_size.y = (FLOAT)texture->vp.h;
 
 		d3d9_vertex_buffer_set(shd, &texture->vp, prev);
 	}
@@ -892,7 +884,7 @@ static BYTE d3d9_texture_create(_texture *texture, UINT index) {
 	}
 
 	if (IDirect3DDevice9_CreateTexture(d3d9.adapter->dev,
-		(UINT) rect->w, (UINT) rect->h, 1,
+		(UINT)rect->w, (UINT)rect->h, 1,
 		D3DUSAGE_RENDERTARGET,
 		sp->fbo_flt ? D3DFMT_A32B32G32R32F : D3DFMT_A8R8G8B8,
 		D3DPOOL_DEFAULT,
@@ -952,8 +944,8 @@ static BYTE d3d9_texture_simple_create(_texture_simple *texture, UINT w, UINT h,
 		}
 	}
 
-	shd->info.video_size.x = (FLOAT) rect->base.w;
-	shd->info.video_size.y = (FLOAT) rect->base.h;
+	shd->info.video_size.x = (FLOAT)rect->base.w;
+	shd->info.video_size.y = (FLOAT)rect->base.h;
 	shd->info.texture_size.x = rect->w;
 	shd->info.texture_size.y = rect->h;
 

src/video/opengl/opengl.c
@@ -34,13 +34,13 @@
 #define BUFFER_VB_OFFSET(a, i) ((char *)&a + (i))
 
 #define _SCR_ROWS_BRD\
-	((float)  (SCR_ROWS - (overscan.borders->left + overscan.borders->right)) * gfx.pixel_aspect_ratio)
+	((float)(SCR_ROWS - (overscan.borders->left + overscan.borders->right)) * gfx.pixel_aspect_ratio)
 #define _SCR_LINES_BRD\
-	(float) (SCR_LINES - (overscan.borders->up + overscan.borders->down))
+	(float)(SCR_LINES - (overscan.borders->up + overscan.borders->down))
 #define _SCR_ROWS_NOBRD\
-	((float) SCR_ROWS * gfx.pixel_aspect_ratio)
+	((float)SCR_ROWS * gfx.pixel_aspect_ratio)
 #define _SCR_LINES_NOBRD\
-	(float) SCR_LINES
+	(float)SCR_LINES
 
 static void opengl_context_delete(void);
 static void opengl_screenshot(void);
@@ -206,34 +206,26 @@ BYTE opengl_context_create(void) {
 
 		if (cfg->fullscreen) {
 			if (!cfg->stretch) {
-				float ratio_frame = (float) gfx.w[VIDEO_MODE] / (float) gfx.h[VIDEO_MODE];
-				float ratio_surface;
+				int mul = gfx.w[VIDEO_MODE] > gfx.h[VIDEO_MODE] ?
+					(gfx.h[VIDEO_MODE] - (gfx.h[VIDEO_MODE] % gfx.h[PASS0])) / gfx.h[PASS0] :
+					(gfx.w[VIDEO_MODE] - (gfx.w[VIDEO_MODE] % gfx.w[PASS0])) / gfx.w[PASS0];
 
-				if (overscan.enabled && (cfg->oscan_black_borders_fscr == FALSE)) {
-					ratio_surface = _SCR_ROWS_BRD / _SCR_LINES_BRD;
-				} else {
-					ratio_surface = _SCR_ROWS_NOBRD / _SCR_LINES_NOBRD;
-				}
-
-				if (ratio_frame > ratio_surface) {
-					vp->w = (int) ((float) gfx.h[VIDEO_MODE] * ratio_surface);
-					vp->x = (int) (((float) gfx.w[VIDEO_MODE] - (float) vp->w) * 0.5f);
-				} else {
-					vp->h = (int) ((float) gfx.w[VIDEO_MODE] / ratio_surface);
-					vp->y = (int) (((float) gfx.h[VIDEO_MODE] - (float) vp->h) * 0.5f);
-				}
+				vp->w = gfx.w[PASS0] * mul;
+				vp->h = gfx.h[PASS0] * mul;
+				vp->x = (gfx.w[VIDEO_MODE] - vp->w) >> 1;
+				vp->y = (gfx.h[VIDEO_MODE] - vp->h) >> 1;
 			}
 
 			if (overscan.enabled && (cfg->oscan_black_borders_fscr == FALSE)) {
 				float brd_l_x, brd_r_x, brd_u_y, brd_d_y;
 				float ratio_x, ratio_y;
 
-				ratio_x = (float) vp->w / _SCR_ROWS_NOBRD;
-				ratio_y = (float) vp->h / _SCR_LINES_NOBRD;
-				brd_l_x = (float) overscan.borders->left * ratio_x;
-				brd_r_x = (float) overscan.borders->right * ratio_x;
-				brd_u_y = (float) overscan.borders->up * ratio_y;
-				brd_d_y = (float) overscan.borders->down * ratio_y;
+				ratio_x = (float)vp->w / _SCR_ROWS_NOBRD;
+				ratio_y = (float)vp->h / _SCR_LINES_NOBRD;
+				brd_l_x = (float)overscan.borders->left * ratio_x;
+				brd_r_x = (float)overscan.borders->right * ratio_x;
+				brd_u_y = (float)overscan.borders->up * ratio_y;
+				brd_d_y = (float)overscan.borders->down * ratio_y;
 
 				vp->x -= brd_l_x;
 				vp->y -= brd_d_y;
@@ -1111,8 +1103,8 @@ INLINE void opengl_shader_filter(uint8_t linear, uint8_t mipmap, uint8_t interpo
 			break;
 		default:
 			(*min) = mipmap ?
-					(interpolation ? GL_LINEAR_MIPMAP_LINEAR : GL_NEAREST_MIPMAP_NEAREST) :
-					(interpolation ? GL_LINEAR : GL_NEAREST);
+				(interpolation ? GL_LINEAR_MIPMAP_LINEAR : GL_NEAREST_MIPMAP_NEAREST) :
+				(interpolation ? GL_LINEAR : GL_NEAREST);
 			break;
 	}
 

commit 0ebcfa818de37a74ee0fb904c2f21c70cd1da058

Fixed 2 bugs introduced with commit https://github.com/punesemu/puNES/commit/008455ebf8af4af9dd62f43ff4fed888e3e44049 where I forgot to take into account the pixel aspect ratio and overscan in fullscreen mode.

src/video/d3d9/d3d9.c
@@ -278,12 +278,13 @@ BYTE d3d9_context_create(void) {
 		// configuro l'aspect ratio del fullscreen
 		if (cfg->fullscreen) {
 			if (!cfg->stretch) {
+				int mw = _SCR_ROWS_NOBRD, mh = _SCR_LINES_NOBRD;
 				int mul = gfx.w[VIDEO_MODE] > gfx.h[VIDEO_MODE] ?
-					(gfx.h[VIDEO_MODE] - (gfx.h[VIDEO_MODE] % gfx.h[PASS0])) / gfx.h[PASS0] :
-					(gfx.w[VIDEO_MODE] - (gfx.w[VIDEO_MODE] % gfx.w[PASS0])) / gfx.w[PASS0];
+					(gfx.h[VIDEO_MODE] - (gfx.h[VIDEO_MODE] % mh)) / mh :
+					(gfx.w[VIDEO_MODE] - (gfx.w[VIDEO_MODE] % mw)) / mw;
 
-				vp->w = gfx.w[PASS0] * mul;
-				vp->h = gfx.h[PASS0] * mul;
+				vp->w = mw * mul;
+				vp->h = mh * mul;
 				vp->x = (gfx.w[VIDEO_MODE] - vp->w) >> 1;
 				vp->y = (gfx.h[VIDEO_MODE] - vp->h) >> 1;
 			}
@@ -385,8 +386,8 @@ BYTE d3d9_context_create(void) {
 			tw = gfx.w[VIDEO_MODE];
 			th = gfx.h[VIDEO_MODE];
 		} else {
-			tw = gfx.w[PASS0] * 2;
-			th = gfx.h[PASS0] * 2;
+			tw = _SCR_ROWS_NOBRD * 2;
+			th = _SCR_LINES_NOBRD * 2;
 		}
 
 		d3d9_texture_simple_create(&d3d9.text, tw, th, TRUE);
@@ -513,10 +514,10 @@ void d3d9_draw_scene(void) {
 		// aggiorno la texture del testo
 		IDirect3DDevice9_UpdateSurface(d3d9.adapter->dev, d3d9.text.offscreen, NULL, d3d9.text.map0, NULL);
 
-		vpx = 0;
-		vpy = 0;
-		vpw = gfx.w[VIDEO_MODE] * gfx.device_pixel_ratio;
-		vph = gfx.h[VIDEO_MODE] * gfx.device_pixel_ratio;
+		vpx = d3d9.viewp.left * gfx.device_pixel_ratio;
+		vpy = d3d9.viewp.top * gfx.device_pixel_ratio;
+		vpw = (d3d9.viewp.right - d3d9.viewp.left) * gfx.device_pixel_ratio;
+		vph = (d3d9.viewp.bottom - d3d9.viewp.top) * gfx.device_pixel_ratio;
 
 		d3d9_viewport_set(vpx, vpy, vpw, vph);
 

src/video/opengl/opengl.c
@@ -206,12 +206,13 @@ BYTE opengl_context_create(void) {
 
 		if (cfg->fullscreen) {
 			if (!cfg->stretch) {
+				int mw = _SCR_ROWS_NOBRD, mh = _SCR_LINES_NOBRD;
 				int mul = gfx.w[VIDEO_MODE] > gfx.h[VIDEO_MODE] ?
-					(gfx.h[VIDEO_MODE] - (gfx.h[VIDEO_MODE] % gfx.h[PASS0])) / gfx.h[PASS0] :
-					(gfx.w[VIDEO_MODE] - (gfx.w[VIDEO_MODE] % gfx.w[PASS0])) / gfx.w[PASS0];
+					(gfx.h[VIDEO_MODE] - (gfx.h[VIDEO_MODE] % mh)) / mh :
+					(gfx.w[VIDEO_MODE] - (gfx.w[VIDEO_MODE] % mw)) / mw;
 
-				vp->w = gfx.w[PASS0] * mul;
-				vp->h = gfx.h[PASS0] * mul;
+				vp->w = mw * mul;
+				vp->h = mh * mul;
 				vp->x = (gfx.w[VIDEO_MODE] - vp->w) >> 1;
 				vp->y = (gfx.h[VIDEO_MODE] - vp->h) >> 1;
 			}
@@ -325,8 +326,8 @@ BYTE opengl_context_create(void) {
 			tw = gfx.w[VIDEO_MODE];
 			th = gfx.h[VIDEO_MODE];
 		} else {
-			tw = gfx.w[PASS0] * 2;
-			th = gfx.h[PASS0] * 2;
+			tw = _SCR_ROWS_NOBRD * 2;
+			th = _SCR_LINES_NOBRD * 2;
 		}
 
 		opengl_texture_simple_create(&opengl.text, tw, th, TRUE);