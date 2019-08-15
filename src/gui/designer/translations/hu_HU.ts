63222ec2a94c375d89f632f3a89ab0931f1d10d0
Fabio Cavallo (FHorse) <punes.development@gmail.com>
2019-08-15 14:00:54 +0200

    New translations en_EN.ts (Hungarian)

--- src/gui/designer/translations/hu_HU.ts
+++ src/gui/designer/translations/hu_HU.ts
@@ -662,6 +662,10 @@
       <source>&amp;Toggle GUI in window</source>
       <translation>GUI ki/be kapcsolása ablakos módban</translation>
     </message>
+    <message>
+      <source>Save Unalte&amp;red NES screen</source>
+      <translation type="unfinished">Save Unalte&amp;red NES screen</translation>
+    </message>
   </context>
   <context>
     <name>objCheat</name>
