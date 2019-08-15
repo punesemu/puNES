f752fdd4bfc92b0b5363ef430f5e7a870231aaeb
Fabio Cavallo (FHorse) <punes.development@gmail.com>
2019-08-15 14:01:08 +0200

    New translations en_EN.ts (Spanish)

--- src/gui/designer/translations/es_ES.ts
+++ src/gui/designer/translations/es_ES.ts
@@ -662,6 +662,10 @@
       <source>&amp;Toggle GUI in window</source>
       <translation>&amp;Cambiar interfaz de la ventana</translation>
     </message>
+    <message>
+      <source>Save Unalte&amp;red NES screen</source>
+      <translation type="unfinished">Save Unalte&amp;red NES screen</translation>
+    </message>
   </context>
   <context>
     <name>objCheat</name>
