1d0a8f1e649da33c7a81d0f850f79b460571af06
Fabio Cavallo (FHorse) <punes.development@gmail.com>
2020-06-27 12:18:57 +0200

    New translations en_EN.ts (Russian)

--- src/gui/designer/translations/ru_RU.ts
+++ src/gui/designer/translations/ru_RU.ts
@@ -1499,6 +1499,10 @@
       <source>Turkish</source>
       <translation>Türkçe</translation>
     </message>
+    <message>
+      <source>Portuguese (Brazil)</source>
+      <translation type="unfinished">Portuguese (Brazil)</translation>
+    </message>
   </context>
   <context>
     <name>wdgSettingsInput</name>
