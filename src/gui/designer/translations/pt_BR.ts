18c991c1faffa0522ed28073e7359aa53df39e9f
Fabio Cavallo (FHorse) <punes.development@gmail.com>
2020-11-17 16:13:08 +0100

    New translations en_EN.ts (Portuguese, Brazilian)

--- src/gui/designer/translations/pt_BR.ts
+++ src/gui/designer/translations/pt_BR.ts
@@ -2394,6 +2394,10 @@
       <source>Also rotate the text on screen</source>
       <translation>Também rotacionar o texto na tela</translation>
     </message>
+    <message>
+      <source>Raw</source>
+      <translation type="unfinished">Raw</translation>
+    </message>
   </context>
   <context>
     <name>wdgState</name>
