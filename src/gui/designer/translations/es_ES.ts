3ed2a65fb50f241987d6112e59717415c443bfe0
Fabio Cavallo (FHorse) <punes.development@gmail.com>
2020-11-18 16:14:25 +0100

    New translations en_EN.ts (Spanish)

--- src/gui/designer/translations/es_ES.ts
+++ src/gui/designer/translations/es_ES.ts
@@ -2371,10 +2371,6 @@
       <translation>Usar escalado en pantalla completa</translation>
     </message>
     <message>
-      <source>Screen rotation</source>
-      <translation>Rotación de la pantalla</translation>
-    </message>
-    <message>
       <source>0 degrees</source>
       <translation>0 grados</translation>
     </message>
@@ -2391,12 +2387,24 @@
       <translation>270 grados</translation>
     </message>
     <message>
-      <source>Also rotate the text on screen</source>
-      <translation>También rotar el texto en pantalla</translation>
-    </message>
-    <message>
       <source>Raw</source>
       <translation type="unfinished">Raw</translation>
+    </message>
+    <message>
+      <source>Horizontal Flip and Screen Rotation</source>
+      <translation type="unfinished">Horizontal Flip and Screen Rotation</translation>
+    </message>
+    <message>
+      <source>Horizontal flip</source>
+      <translation type="unfinished">Horizontal flip</translation>
+    </message>
+    <message>
+      <source>Flip/Rotate the controller input</source>
+      <translation type="unfinished">Flip/Rotate the controller input</translation>
+    </message>
+    <message>
+      <source>Flip/Rotate the text on screen</source>
+      <translation type="unfinished">Flip/Rotate the text on screen</translation>
     </message>
   </context>
   <context>
