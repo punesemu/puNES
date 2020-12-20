598eb4fb6ab896840e984326bdfac5896d46ec4d
Fabio Cavallo (FHorse) <punes.development@gmail.com>
2020-12-20 11:51:13 +0100

    New translations en_EN.ts (Italian)

--- src/gui/designer/translations/it_IT.ts
+++ src/gui/designer/translations/it_IT.ts
@@ -1449,10 +1449,6 @@
       <translation>Configurazione Generale</translation>
     </message>
     <message>
-      <source>2x (Default)</source>
-      <translation>2x (Predefinito)</translation>
-    </message>
-    <message>
       <source>3x</source>
       <translation>3x</translation>
     </message>
@@ -1541,30 +1537,6 @@
       <translation>Minuti del rewind</translation>
     </message>
     <message>
-      <source>Disabled</source>
-      <translation>Disabilitato</translation>
-    </message>
-    <message>
-      <source>2 minutes</source>
-      <translation>2 minuti</translation>
-    </message>
-    <message>
-      <source>5 minutes</source>
-      <translation>5 minuti</translation>
-    </message>
-    <message>
-      <source>15 minutes (Default)</source>
-      <translation>15 minuti (Predefinito)</translation>
-    </message>
-    <message>
-      <source>30 minutes</source>
-      <translation>30 minuti</translation>
-    </message>
-    <message>
-      <source>Unlimited</source>
-      <translation>Illimitati</translation>
-    </message>
-    <message>
       <source>Spanish</source>
       <translation>Spagnolo</translation>
     </message>
@@ -1573,16 +1545,44 @@
       <translation>Ungherese</translation>
     </message>
     <message>
-      <source>60 minutes</source>
-      <translation>60 minuti</translation>
-    </message>
-    <message>
       <source>Turkish</source>
       <translation>Turco</translation>
     </message>
     <message>
       <source>Portuguese (Brazil)</source>
       <translation>Portoghese (Brasile)</translation>
+    </message>
+    <message>
+      <source>Off</source>
+      <translation type="unfinished">Off</translation>
+    </message>
+    <message>
+      <source>2</source>
+      <translation type="unfinished">2</translation>
+    </message>
+    <message>
+      <source>5</source>
+      <translation type="unfinished">5</translation>
+    </message>
+    <message>
+      <source>15</source>
+      <translation type="unfinished">15</translation>
+    </message>
+    <message>
+      <source>30</source>
+      <translation type="unfinished">30</translation>
+    </message>
+    <message>
+      <source>60</source>
+      <translation type="unfinished">60</translation>
+    </message>
+    <message>
+      <source>Unlim.</source>
+      <translation type="unfinished">Unlim.</translation>
+    </message>
+    <message>
+      <source>2x</source>
+      <translation type="unfinished">2x</translation>
     </message>
   </context>
   <context>
