ac662c4642066f46470107e580463413f895662a
Fabio Cavallo (FHorse) <punes.development@gmail.com>
2020-12-27 16:32:25 +0100

    New translations en_EN.ts (Spanish)

--- src/gui/designer/translations/es_ES.ts
+++ src/gui/designer/translations/es_ES.ts
@@ -709,6 +709,11 @@
       <source>Start/Stop &amp;VIDEO recording</source>
       <translation>Iniciar/detener grabación de &amp;VÍDEO</translation>
     </message>
+    <message>
+      <source>Slot %1 never used</source>
+      <extracomment>Refers to the unused save slot. Important: Do not translate the &quot;%1&quot;.</extracomment>
+      <translation type="unfinished">Slot %1 never used</translation>
+    </message>
   </context>
   <context>
     <name>objCheat</name>
@@ -2312,10 +2317,6 @@
     <message>
       <source>Save/Load Slot Box</source>
       <translation>Guardar/cargar cartucho de ranura</translation>
-    </message>
-    <message>
-      <source>Slot %1</source>
-      <translation>Ranura %1</translation>
     </message>
     <message>
       <source>Save state</source>
