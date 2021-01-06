6e8507057960c14ac7a2a267fd5f4a51a2b76dd1
Fabio Cavallo (FHorse) <punes.development@gmail.com>
2021-01-06 16:44:38 +0100

    New translations en_EN.ts (Spanish)

--- src/gui/designer/translations/es_ES.ts
+++ src/gui/designer/translations/es_ES.ts
@@ -2315,6 +2315,10 @@
       <source>Palette File</source>
       <translation>Archivo de paleta</translation>
     </message>
+    <message>
+      <source>Show frames and lags counters</source>
+      <translation type="unfinished">Show frames and lags counters</translation>
+    </message>
   </context>
   <context>
     <name>wdgState</name>
