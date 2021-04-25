4f56a379063ebbf797b6b682aa47ca60f3f3557a
Fabio Cavallo (FHorse) <punes.development@gmail.com>
2021-04-25 22:50:22 +0200

    New translations en_EN.ts (Russian)

--- src/gui/designer/translations/ru_RU.ts
+++ src/gui/designer/translations/ru_RU.ts
@@ -378,10 +378,6 @@
       <translation>Скомпилирован</translation>
     </message>
     <message>
-      <source>Anyway, thank you all for the love and the help.</source>
-      <translation>Спасибо всем за отзывы и поддержку.</translation>
-    </message>
-    <message>
       <source>portable version</source>
       <translation>портативная версия</translation>
     </message>
@@ -656,10 +652,6 @@
     <message>
       <source>Open IPS/BPS/XDELTA Patch</source>
       <translation>Открыть IPS/BPS/XDELTA патч</translation>
-    </message>
-    <message>
-      <source>If you like the emulator and you want to support it&apos;s development or would you pay for a beer at the programmer</source>
-      <translation>Если вам понравился эмулятор, и вы хотите поддержать разработку или скинуться программисту на пиво</translation>
     </message>
     <message>
       <source>&amp;Apply IPS/BPS/XDELTA Patch</source>
