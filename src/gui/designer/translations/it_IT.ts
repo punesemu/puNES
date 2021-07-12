990096a8e3e28cc4e2dfefb23874fe537ed4c3b1
Fabio Cavallo (FHorse) <punes.development@gmail.com>
2021-07-12 15:23:27 +0200

    New translations en_EN.ts (Italian)

--- src/gui/designer/translations/it_IT.ts
+++ src/gui/designer/translations/it_IT.ts
@@ -860,9 +860,14 @@
       <translation>[red]errore nel file della paletta[normal]</translation>
     </message>
     <message>
-      <source>[red]errors[normal] on shader, use [green]&apos;No shader&apos;[normal]</source>
+      <source>[red]errors[normal] on shader, use [green]&apos;No shader&apos;[cyan]</source>
       <extracomment>Do not translate the words contained between parentheses (example: [red] or [normal]) are tags that have a specific meaning and do not traslate %1 and %2</extracomment>
-      <translation>[red]errori[normal] nella shader, imposto [green]&apos;No shader&apos;[normal]</translation>
+      <translation type="unfinished">[red]errors[normal] on shader, use [green]&apos;No shader&apos;[cyan]</translation>
+    </message>
+    <message>
+      <source>switch on [cyan]%1x%2[normal] at [green]%3Hz[normal]</source>
+      <extracomment>Do not translate the words contained between parentheses (example: [red] or [normal]) are tags that have a specific meaning and do not traslate %1 and %2</extracomment>
+      <translation type="unfinished">switch on [cyan]%1x%2[normal] at [green]%3Hz[normal]</translation>
     </message>
   </context>
   <context>
@@ -2232,10 +2237,6 @@
       <translation>Disabilita scambio dell&apos;emphsis tra Rosso e Verde nella modalità PAL/Dendy</translation>
     </message>
     <message>
-      <source>Use integer scaling in fullscreen</source>
-      <translation>Usa ridimensionamento non frazionato a schermo intero</translation>
-    </message>
-    <message>
       <source>0 degrees</source>
       <translation>0 gradi</translation>
     </message>
@@ -2310,6 +2311,30 @@
     <message>
       <source>Show frames and lags counters</source>
       <translation type="unfinished">Show frames and lags counters</translation>
+    </message>
+    <message>
+      <source>Desktop resolution</source>
+      <translation type="unfinished">Desktop resolution</translation>
+    </message>
+    <message>
+      <source>Adapt the refresh rate to the region (PAL/Dendy 50Hz, NTSC 60Hz)</source>
+      <translation type="unfinished">Adapt the refresh rate to the region (PAL/Dendy 50Hz, NTSC 60Hz)</translation>
+    </message>
+    <message>
+      <source>Fullscreen</source>
+      <translation type="unfinished">Fullscreen</translation>
+    </message>
+    <message>
+      <source>* applied if supported by the monitor</source>
+      <translation type="unfinished">* applied if supported by the monitor</translation>
+    </message>
+    <message>
+      <source>Use integer scaling</source>
+      <translation type="unfinished">Use integer scaling</translation>
+    </message>
+    <message>
+      <source>Preferred resolution </source>
+      <translation type="unfinished">Preferred resolution </translation>
     </message>
   </context>
   <context>
