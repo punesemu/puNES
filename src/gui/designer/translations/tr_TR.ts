18c501b17bbd9d3d61bd7054b0942f5e9a19882c
Fabio Cavallo (FHorse) <punes.development@gmail.com>
2021-12-15 11:25:44 +0100

    New translations en_EN.ts (Turkish)

--- src/gui/designer/translations/tr_TR.ts
+++ src/gui/designer/translations/tr_TR.ts
@@ -25,6 +25,10 @@
       <source>Close Window</source>
       <translation>Pencereyi Kapat</translation>
     </message>
+    <message>
+      <source>Save Configuration</source>
+      <translation type="unfinished">Save Configuration</translation>
+    </message>
   </context>
   <context>
     <name>dlgSettings</name>
