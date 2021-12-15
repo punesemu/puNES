39c75787036b4103bcbc8e5d8a7ca9e1ac797330
Fabio Cavallo (FHorse) <punes.development@gmail.com>
2021-12-15 11:25:46 +0100

    New translations en_EN.ts (Portuguese, Brazilian)

--- src/gui/designer/translations/pt_BR.ts
+++ src/gui/designer/translations/pt_BR.ts
@@ -25,6 +25,10 @@
       <source>Close Window</source>
       <translation>Fechar Janela</translation>
     </message>
+    <message>
+      <source>Save Configuration</source>
+      <translation type="unfinished">Save Configuration</translation>
+    </message>
   </context>
   <context>
     <name>dlgSettings</name>
