093db554872d1b290893498b90d0695d0691f224
fhorse <punes.development@gmail.com>
2022-01-31 14:26:32 +0100

    It's more correct to use the setGeometry() using only the x and y coordinates of the original geometry.

--- src/gui/mainWindow.cpp
+++ src/gui/mainWindow.cpp
@@ -1822,7 +1822,7 @@ void mainWindow::s_fullscreen(void) {
 		fullscreen_resize = false;
 		showNormal();
 		gfx_set_screen(gfx.scale_before_fscreen, NO_CHANGE, NO_CHANGE, NO_FULLSCR, NO_CHANGE, FALSE, FALSE);
-		move(org_geom.x(), org_geom.y());
+		setGeometry(org_geom.x(), org_geom.y(), geometry().width(), geometry().height());
 	}
 
 	emu_thread_continue();
