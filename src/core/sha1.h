634e6a2874fbc0497621e597c71004a11ab39e98
fhorse <punes.development@gmail.com>
2022-03-28 00:20:35 +0200

    Updated sha1.c and sha1.h.
    This should fix the compilation error with libc musl
    https://bugs.gentoo.org/show_bug.cgi?id=830471.

--- src/core/sha1.h
+++ src/core/sha1.h
@@ -1,64 +1,123 @@
-/*
- *  Copyright (C) 2010-2022 Fabio Cavallo (aka FHorse)
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+/**
+ * \file sha1.h
  */
+#ifndef XYSSL_SHA1_H
+#define XYSSL_SHA1_H
 
-#ifndef SHA1_H_
-#define SHA1_H_
+/**
+ * \brief          SHA-1 context structure
+ */
+typedef struct
+{
+    unsigned long total[2];     /*!< number of bytes processed  */
+    unsigned long state[5];     /*!< intermediate digest state  */
+    unsigned char buffer[64];   /*!< data block being processed */
 
-#include "common.h"
+    unsigned char ipad[64];     /*!< HMAC: inner padding        */
+    unsigned char opad[64];     /*!< HMAC: outer padding        */
+}
+sha1_context;
 
-#ifndef _STD_TYPES
-#define _STD_TYPES
-#define uchar unsigned char
-#define uint  unsigned int
-#define ulong unsigned long int
+#ifdef __cplusplus
+extern "C" {
 #endif
 
-typedef struct {
-	ulong total[2];
-	ulong state[5];
-	uchar buffer[64];
-} sha1_context;
-
-/*
- * Core SHA-1 functions
+/**
+ * \brief          SHA-1 context setup
+ *
+ * \param ctx      context to be initialized
  */
-void sha1_starts(sha1_context *ctx);
-void sha1_update(sha1_context *ctx, uchar *input, uint length);
-void sha1_finish(sha1_context *ctx, uchar digest[20]);
+void sha1_starts( sha1_context *ctx );
 
-/*
- * Output SHA-1(file contents), returns 0 if successful.
+/**
+ * \brief          SHA-1 process buffer
+ *
+ * \param ctx      SHA-1 context
+ * \param input    buffer holding the  data
+ * \param ilen     length of the input data
  */
-int sha1_file(char *filename, uchar digest[20], char *string);
+void sha1_update( sha1_context *ctx, unsigned char *input, int ilen );
 
-/*
- * Output SHA-1(buf)
+/**
+ * \brief          SHA-1 final digest
+ *
+ * \param ctx      SHA-1 context
+ * \param output   SHA-1 checksum result
  */
-void sha1_csum(uchar *buf, uint buflen, uchar digest[20], char *string, int typecase);
+void sha1_finish( sha1_context *ctx, unsigned char output[20] );
 
-/*
- * Output HMAC-SHA-1(key,buf)
+/**
+ * \brief          Output = SHA-1( input buffer )
+ *
+ * \param input    buffer holding the  data
+ * \param ilen     length of the input data
+ * \param output   SHA-1 checksum result
  */
-void sha1_hmac(uchar *key, uint keylen, uchar *buf, uint buflen, uchar digest[20]);
+void sha1( unsigned char *input, int ilen, unsigned char output[20] );
 
-/*
- * Checkup routine
+/**
+ * \brief          Output = SHA-1( file contents )
+ *
+ * \param path     input file name
+ * \param output   SHA-1 checksum result
+ *
+ * \return         0 if successful, 1 if fopen failed,
+ *                 or 2 if fread failed
  */
-int sha1_self_test(void);
+int sha1_file( char *path, unsigned char output[20] );
 
-#endif /* SHA1_H_ */
+/**
+ * \brief          SHA-1 HMAC context setup
+ *
+ * \param ctx      HMAC context to be initialized
+ * \param key      HMAC secret key
+ * \param keylen   length of the HMAC key
+ */
+void sha1_hmac_starts( sha1_context *ctx, unsigned char *key, int keylen );
+
+/**
+ * \brief          SHA-1 HMAC process buffer
+ *
+ * \param ctx      HMAC context
+ * \param input    buffer holding the  data
+ * \param ilen     length of the input data
+ */
+void sha1_hmac_update( sha1_context *ctx, unsigned char *input, int ilen );
+
+/**
+ * \brief          SHA-1 HMAC final digest
+ *
+ * \param ctx      HMAC context
+ * \param output   SHA-1 HMAC checksum result
+ */
+void sha1_hmac_finish( sha1_context *ctx, unsigned char output[20] );
+
+/**
+ * \brief          Output = HMAC-SHA-1( hmac key, input buffer )
+ *
+ * \param key      HMAC secret key
+ * \param keylen   length of the HMAC key
+ * \param input    buffer holding the  data
+ * \param ilen     length of the input data
+ * \param output   HMAC-SHA-1 result
+ */
+void sha1_hmac( unsigned char *key, int keylen,
+                unsigned char *input, int ilen,
+                unsigned char output[20] );
+
+/**
+ * \brief          Checkup routine
+ *
+ * \return         0 if successful, or 1 if the test failed
+ */
+int sha1_self_test( int verbose );
+
+// FHorse mod
+void sha1_csum(unsigned char *buf, int buflen, unsigned char digest[20], char *string, int typecase);
+// FHorse mod end
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* sha1.h */
