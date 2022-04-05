punesemu/puNES

commit 563b9821efbfe2713b94d2c84ac556f20e9d5cc8

Fixed https://github.com/punesemu/puNES/issues/195.

src/gui/mainWindow.cpp
@@ -214,6 +214,14 @@ mainWindow::mainWindow() : QMainWindow() {
 	toolbar->setHidden(cfg->toolbar.hidden);
 	statusbar->setHidden(cfg->toolbar.hidden);
 
+	{
+		bool visibility = !info.start_with_hidden_gui;
+
+		menubar->setVisible(visibility);
+		toolbar->setVisible(visibility);
+		statusbar->setVisible(visibility);
+	}
+
 	set_language(cfg->language);
 }
 mainWindow::~mainWindow() {}

src/gui/qt.cpp
@@ -226,9 +226,6 @@ void gui_start(void) {
 	fps.frame.expected_end = gui_get_ms() + machine.ms_frame;
 	gfx_thread_continue();
 	emu_thread_continue();
-	if (info.start_with_hidden_gui) {
-		QTimer::singleShot(75, []() { qt.mwin->action_Toggle_GUI_in_window->trigger(); });
-	}
 	qt.app->exec();
 }
 

src/video/d3d9/d3d9.c
@@ -323,7 +323,11 @@ BYTE d3d9_context_create(void) {
 	IDirect3DDevice9_SetTransform(d3d9.adapter->dev, D3DTS_VIEW, &identity);
 
 	// screen
-	d3d9_texture_simple_create(&d3d9.screen.tex[0], w, h, FALSE);
+	if (d3d9_texture_simple_create(&d3d9.screen.tex[0], w, h, FALSE) == EXIT_ERROR) {
+		d3d9_context_delete(FALSE);
+		gfx_thread_unlock();
+		return (EXIT_ERROR);
+	}
 
 	// lut (devo farlo prima di elaborare le shaders)
 	for (i = 0; i < shader_effect.luts; i++) {
@@ -502,7 +506,11 @@ BYTE d3d9_context_create(void) {
 
 	// PREV
 	for (i = 1; i < d3d9.screen.in_use; i++) {
-		d3d9_texture_simple_create(&d3d9.screen.tex[i], w, h, FALSE);
+		if (d3d9_texture_simple_create(&d3d9.screen.tex[i], w, h, FALSE) == EXIT_ERROR) {
+			d3d9_context_delete(FALSE);
+			gfx_thread_unlock();
+			return (EXIT_ERROR);
+		}
 	}
 
 	// FEEDBACK
@@ -572,7 +580,11 @@ BYTE d3d9_context_create(void) {
 			oh = tmp;
 		}
 
-		d3d9_texture_simple_create(&d3d9.overlay, ow, oh, TRUE);
+		if (d3d9_texture_simple_create(&d3d9.overlay, ow, oh, TRUE) == EXIT_ERROR) {
+			d3d9_context_delete(FALSE);
+			gfx_thread_unlock();
+			return (EXIT_ERROR);
+		}
 
 		gui_overlay_set_size(ow, oh);
 

src/video/gfx.c
@@ -369,10 +369,10 @@ void gfx_set_screen(BYTE scale, DBWORD filter, DBWORD shader, BYTE fullscreen, B
 		if (shaders_set(shader) == EXIT_ERROR) {
 			umemcpy(cfg->shader_file, gfx.last_shader_file, usizeof(cfg->shader_file));
 			if (old_shader == shader) {
-				fprintf(stderr, "OPENGL: Error on loading the shader, switch to \"No shader\"\n");
+				fprintf(stderr, "GFX: Error on loading the shader, switch to \"No shader\"\n");
 				shader = NO_SHADER;
 			} else {
-				fprintf(stderr, "OPENGL: Error on loading the shader, switch to previous shader\n");
+				fprintf(stderr, "GFX: Error on loading the shader, switch to previous shader\n");
 				shader = old_shader;
 			}
 			goto gfx_set_screen_start;
@@ -414,9 +414,10 @@ void gfx_set_screen(BYTE scale, DBWORD filter, DBWORD shader, BYTE fullscreen, B
 
 		switch (gfx_api_context_create()) {
 			case EXIT_ERROR:
-				fprintf(stderr, "Unable to initialize opengl context\n");
+				fprintf(stderr, "Unable to initialize gfx context\n");
+				info.stop = TRUE;
 				gfx_thread_continue();
-				break;
+				return;
 			case EXIT_ERROR_SHADER:
 				gui_overlay_info_append_msg_precompiled(27, NULL);
 				fprintf(stderr, "Error on loading the shader, switch to \"No shader\"\n");
@@ -560,7 +561,7 @@ void gfx_apply_filter(void) {
 
 	// posso trovarmi nella situazione in cui applico il filtro ad un frame quando ancora
 	// (per molteplici motivi) non ho ancora finito di disegnare il frame precedente. Il gui_screen_update
-	// mettere in coda un'altro update che verrebbe eseguito sempre sullo stesso frame disegnandolo
+	// metterebbe in coda un'altro update che verrebbe eseguito sempre sullo stesso frame disegnandolo
 	// a video due volte.
 	if ((gfx.frame.filtered - gfx.frame.in_draw) != 2) {
 		gui_screen_update();

commit 164346991c3a820db6cb27c2f521230104a99b0d

Fixed a bug introduced in the commit https://github.com/punesemu/puNES/commit/563b9821efbfe2713b94d2c84ac556f20e9d5cc8 for which the "toolbar hidden" option no longer had any effect.

src/gui/mainWindow.cpp
@@ -209,15 +209,12 @@ mainWindow::mainWindow() : QMainWindow() {
 
 	installEventFilter(this);
 
-	toolbar->setHidden(cfg->toolbar.hidden);
-	statusbar->setHidden(cfg->toolbar.hidden);
-
 	{
-		bool visibility = !info.start_with_hidden_gui;
+		bool visibile = !info.start_with_hidden_gui;
 
-		menubar->setVisible(visibility);
-		toolbar->setVisible(visibility);
-		statusbar->setVisible(visibility);
+		menubar->setVisible(visibile);
+		toolbar->setVisible(visibile ? !cfg->toolbar.hidden : false);
+		statusbar->setVisible(visibile ? !cfg->toolbar.hidden : false);
 	}
 
 	set_language(cfg->language);