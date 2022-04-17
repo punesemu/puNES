punesemu/puNES

commit 0035ef3c2e4ad7d11d05bcb0af796817d17a5aeb

Added support for mapper 268 (UNIF : COOLBOY e MINDKIDS).
Also updated mapper_8_in_1, mapper_BMCFK23C, mapper_BOY and
mapper_SA_9602B.

src/core/ines.c
@@ -283,28 +283,11 @@ BYTE ines_load_rom(void) {
 			if (info.format == NES_2_0) {
 				info.chr.rom[0].banks_8k = nes20_ram_size(ines.flags[FL11] & 0x0F);
 			}
-			if (!info.chr.rom[0].banks_8k) {
-				if (info.format == iNES_1_0) {
-					if (info.extra_from_db & CHRRAM32K) {
-						info.chr.rom[0].banks_8k = 4;
-					} else if (info.extra_from_db & CHRRAM256K) {
-						info.chr.rom[0].banks_8k = 32;
-					} else {
-						info.chr.rom[0].banks_8k = 1;
-					}
-				} else {
-					info.chr.rom[0].banks_8k = 1;
-				}
-			}
 		}
 
 		info.prg.rom[0].banks_8k = !info.prg.rom[0].banks_16k ? 1 : info.prg.rom[0].banks_16k * 2;
-		info.chr.rom[0].banks_4k = info.chr.rom[0].banks_8k * 2;
-		info.chr.rom[0].banks_1k = info.chr.rom[0].banks_4k * 4;
-
-		map_set_banks_max_prg(0);
-		map_set_banks_max_chr(0);
 		info.prg.chips = info.chr.chips = 0;
+		map_set_banks_max_prg(0);
 
 		// alloco la PRG Ram
 		if (map_prg_ram_malloc(0x2000) != EXIT_OK) {
@@ -628,6 +611,16 @@ void nes20_submapper(void) {
 			break;
 		case 210:
 			search_in_database();
+			break;
+		case 268:
+			switch (info.mapper.submapper) {
+				case 0:
+					info.mapper.submapper = COOLBOY;
+					break;
+				case 1:
+					info.mapper.submapper = MINDKIDS;
+					break;
+			}
 			break;
 	}
 }

src/core/Makefile.am
@@ -341,6 +341,8 @@ libcore_a_SOURCES += \
 	mappers/mapper_CNROM.h \
 	mappers/mapper_ColorDreams.c \
 	mappers/mapper_ColorDreams.h \
+	mappers/mapper_Coolboy.c \
+	mappers/mapper_Coolboy.h \
 	mappers/mapper_CPROM.c \
 	mappers/mapper_CPROM.h \
 	mappers/mapper_DRAGONFIGHTER.c \

src/core/mappers.c
@@ -710,6 +710,9 @@ BYTE map_init(void) {
 		case 254:
 			map_init_254();
 			break;
+		case 268:
+			map_init_Coolboy(info.mapper.submapper == MINDKIDS ? MINDKIDS : COOLBOY);
+			break;
 		case 521:
 			map_init_DREAMTECH01();
 			break;
@@ -957,11 +960,35 @@ BYTE map_init(void) {
 			}
 			break;
 	}
+
+	// PRG
 	map_prg_rom_8k_update();
 	map_prg_ram_init();
+
+	// CHR
+	if (mapper.write_vram == TRUE) {
+		if (!info.chr.rom[0].banks_8k) {
+			if ((info.format == iNES_1_0) || (info.format == UNIF_FORMAT)) {
+				if (info.extra_from_db & CHRRAM32K) {
+					info.chr.rom[0].banks_8k = 4;
+				} else if (info.extra_from_db & CHRRAM256K) {
+					info.chr.rom[0].banks_8k = 32;
+				} else {
+					info.chr.rom[0].banks_8k = 1;
+				}
+			} else {
+				info.chr.rom[0].banks_8k = 1;
+			}
+		}
+		info.chr.rom[0].banks_4k = info.chr.rom[0].banks_8k * 2;
+		info.chr.rom[0].banks_1k = info.chr.rom[0].banks_4k * 4;
+		map_set_banks_max_chr(0);
+	}
 	if (map_chr_ram_init()) {
 		return (EXIT_ERROR);
 	}
+
+	// after mapper init
 	if (extcl_after_mapper_init) {
 		extcl_after_mapper_init();
 	}

src/core/mappers.h
@@ -33,6 +33,7 @@
 #include "mappers/mapper_CNROM.h"
 #include "mappers/mapper_AxROM.h"
 #include "mappers/mapper_ColorDreams.h"
+#include "mappers/mapper_Coolboy.h"
 #include "mappers/mapper_Camerica.h"
 #include "mappers/mapper_CPROM.h"
 #include "mappers/mapper_BxROM.h"

src/core/mappers/mapper_8_IN_1.c
@@ -84,6 +84,7 @@ struct _m8in1 {
 } m8in1;
 
 void map_init_8_IN_1(void) {
+	EXTCL_AFTER_MAPPER_INIT(8_IN_1);
 	EXTCL_CPU_WR_MEM(8_IN_1);
 	EXTCL_SAVE_MAPPER(8_IN_1);
 	EXTCL_CPU_EVERY_CYCLE(MMC3);
@@ -101,25 +102,28 @@ void map_init_8_IN_1(void) {
 	memset(&mmc3, 0x00, sizeof(mmc3));
 	memset(&irqA12, 0x00, sizeof(irqA12));
 
-	{
-		BYTE i;
-
-		map_prg_rom_8k_reset();
-		map_chr_bank_1k_reset();
-
-		for (i = 0; i < 8; i++) {
-			if (i < 4) {
-				m8in1.prg_map[i] = mapper.rom_map_to[i];
-			}
-			m8in1.chr_map[i] = i;
-		}
-
-		m8in1_update_prg();
-		m8in1_update_chr();
+	if (mapper.write_vram == TRUE) {
+		info.chr.rom[0].banks_8k = 32;
 	}
 
 	irqA12.present = TRUE;
 	irqA12_delay = 1;
+}
+void extcl_after_mapper_init_8_IN_1(void) {
+	BYTE i;
+
+	map_prg_rom_8k_reset();
+	map_chr_bank_1k_reset();
+
+	for (i = 0; i < 8; i++) {
+		if (i < 4) {
+			m8in1.prg_map[i] = mapper.rom_map_to[i];
+		}
+		m8in1.chr_map[i] = i;
+	}
+
+	m8in1_update_prg();
+	m8in1_update_chr();
 }
 void extcl_cpu_wr_mem_8_IN_1(WORD address, BYTE value) {
 	BYTE old_prg_rom_cfg = mmc3.prg_rom_cfg;

src/core/mappers/mapper_8_IN_1.h
@@ -22,6 +22,7 @@
 #include "common.h"
 
 void map_init_8_IN_1(void);
+void extcl_after_mapper_init_8_IN_1(void);
 void extcl_cpu_wr_mem_8_IN_1(WORD address, BYTE value);
 BYTE extcl_save_mapper_8_IN_1(BYTE mode, BYTE slot, FILE *fp);
 

src/core/mappers/mapper_BMCFK23C.c
@@ -23,10 +23,10 @@
 #include "irqA12.h"
 #include "save_slot.h"
 
-static void prg_fix_BMCFK23CPW(BYTE value);
-static void prg_swap_BMCFK23CPW(WORD address, WORD value);
-static void chr_fix_BMCFK23CPW(BYTE value);
-static void chr_swap_BMCFK23CCW(WORD address, WORD value);
+INLINE static void prg_fix_BMCFK23CPW(BYTE value);
+INLINE static void prg_swap_BMCFK23CPW(WORD address, WORD value);
+INLINE static void chr_fix_BMCFK23CPW(BYTE value);
+INLINE static void chr_swap_BMCFK23CCW(WORD address, WORD value);
 
 struct _bmcfk23c {
 	uint32_t dipswitch;
@@ -41,6 +41,7 @@ struct _bmcfk23c {
 } bmcfk23c;
 
 void map_init_BMCFK23C(void) {
+	EXTCL_AFTER_MAPPER_INIT(BMCFK23C);
 	EXTCL_CPU_WR_MEM(BMCFK23C);
 	EXTCL_SAVE_MAPPER(BMCFK23C);
 	EXTCL_CPU_EVERY_CYCLE(MMC3);
@@ -103,13 +104,15 @@ void map_init_BMCFK23C(void) {
 		bmcfk23c.prg_mask = 0x7F >> prg_bonus;
 	}
 
-	prg_fix_BMCFK23CPW(mmc3.bank_to_update);
-	chr_fix_BMCFK23CPW(mmc3.bank_to_update);
-
 	info.mapper.extend_wr = TRUE;
 
 	irqA12.present = TRUE;
 	irqA12_delay = 1;
+}
+void extcl_after_mapper_init_BMCFK23C(void) {
+	// posso farlo solo dopo il map_prg_ram_init();
+	prg_fix_BMCFK23CPW(mmc3.bank_to_update);
+	chr_fix_BMCFK23CPW(mmc3.bank_to_update);
 }
 void extcl_cpu_wr_mem_BMCFK23C(WORD address, BYTE value) {
 	if ((address >= 0x5000) && (address <= 0x5FFF)) {
@@ -185,18 +188,10 @@ void extcl_cpu_wr_mem_BMCFK23C(WORD address, BYTE value) {
 					// check if it not interfer with other dumps
 					if (address == 0x8000) {
 						if (value == 0x46) {
-
-
 							fprintf(stderr, "0x%04X : 0x%02X\n", address, value);
-
-
 							value = 0x47;
 						} else if (value == 0x47) {
-
-
 							fprintf(stderr, "0x%04X : 0x%02X\n", address, value);
-
-
 							value = 0x46;
 						}
 					}
@@ -297,7 +292,7 @@ BYTE extcl_save_mapper_BMCFK23C(BYTE mode, BYTE slot, FILE *fp) {
 	return (EXIT_OK);
 }
 
-static void prg_fix_BMCFK23CPW(BYTE value) {
+INLINE static void prg_fix_BMCFK23CPW(BYTE value) {
 	if (value & 0x40) {
 		prg_swap_BMCFK23CPW(0x8000, ~1);
 		prg_swap_BMCFK23CPW(0xC000, bmcfk23c.mmc3[6]);
@@ -308,7 +303,7 @@ static void prg_fix_BMCFK23CPW(BYTE value) {
 	prg_swap_BMCFK23CPW(0xA000, bmcfk23c.mmc3[7]);
 	prg_swap_BMCFK23CPW(0xE000, ~0);
 }
-static void prg_swap_BMCFK23CPW(WORD address, WORD value) {
+INLINE static void prg_swap_BMCFK23CPW(WORD address, WORD value) {
 	if ((bmcfk23c.reg[0] & 0x07) == 0x04) {
 		value = bmcfk23c.reg[1] >> 1;
 		control_bank(info.prg.rom[0].max.banks_32k)
@@ -348,7 +343,7 @@ static void prg_swap_BMCFK23CPW(WORD address, WORD value) {
 	//setprg8r(0x10, 0x6000, A001B & 3);
 }
 
-static void chr_fix_BMCFK23CPW(BYTE value) {
+INLINE static void chr_fix_BMCFK23CPW(BYTE value) {
 	WORD cbase = (value & 0x80) << 5;
 
 	chr_swap_BMCFK23CCW(cbase ^ 0x0000, bmcfk23c.mmc3[0] & (~1));
@@ -361,7 +356,7 @@ static void chr_fix_BMCFK23CPW(BYTE value) {
 	chr_swap_BMCFK23CCW(cbase ^ 0x1800, bmcfk23c.mmc3[4]);
 	chr_swap_BMCFK23CCW(cbase ^ 0x1c00, bmcfk23c.mmc3[5]);
 }
-static void chr_swap_BMCFK23CCW(WORD address, WORD value) {
+INLINE static void chr_swap_BMCFK23CCW(WORD address, WORD value) {
 	DBWORD bank;
 
 	if (bmcfk23c.reg[0] & 0x40) {
@@ -424,4 +419,3 @@ static void chr_swap_BMCFK23CCW(WORD address, WORD value) {
 		}
 	}
 }
-

src/core/mappers/mapper_BMCFK23C.h
@@ -24,6 +24,7 @@
 enum bmcfk23c_types { NOBMCFK23C, BMCFK23C_0 = 1, BMCFK23C_1 = 2, BMCFK23CA = 8 };
 
 void map_init_BMCFK23C(void);
+void extcl_after_mapper_init_BMCFK23C(void);
 void extcl_cpu_wr_mem_BMCFK23C(WORD address, BYTE value);
 void extcl_wr_chr_BMCFK23C(WORD address, BYTE value);
 BYTE extcl_save_mapper_BMCFK23C(BYTE mode, BYTE slot, FILE *fp);

commit 1d91fd739fe118c2efc684456d146a007d94b420

Fixed regression introduced with https://github.com/punesemu/puNES/commit/0035ef3c2e4ad7d11d05bcb0af796817d17a5aeb.

src/core/ines.c
@@ -326,6 +326,9 @@ BYTE ines_load_rom(void) {
 				return (EXIT_ERROR);
 			}
 
+			info.chr.rom[0].banks_4k = info.chr.rom[0].banks_8k * 2;
+			info.chr.rom[0].banks_1k = info.chr.rom[0].banks_4k * 4;
+			map_set_banks_max_chr(0);
 			map_chr_bank_1k_reset();
 
 			if (info.format == NES_2_0) {