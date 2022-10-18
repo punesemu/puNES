punesemu/puNES

commit 3c7b77be71c12792ea6cc0b02f7c280a38e74c79

Rewritten the management of the virtual keyboard.
Redesigned the "Family Basic Keyboard".
Added a setup mode to assign keys.
Added an option to choose the size of the virtual keyboard.
A compiler that supports C ++ 17 is required to use the "qkeycode"
library (https://github.com/nyanpasu64/qkeycode).

src/gui/settings.h
@@ -276,7 +276,82 @@ enum inp_element {
 
 	SET_INP_CONTROLLER_MODE,
 	SET_INP_LEFTRIGHT,
-	SET_INP_HIDE_ZAPPER_CURSOR
+	SET_INP_HIDE_ZAPPER_CURSOR,
+
+	SET_INP_VK_SIZE,
+
+	SET_INP_FBK_0,
+	SET_INP_FBK_1,
+	SET_INP_FBK_2,
+	SET_INP_FBK_3,
+	SET_INP_FBK_4,
+	SET_INP_FBK_5,
+	SET_INP_FBK_6,
+	SET_INP_FBK_7,
+	SET_INP_FBK_8,
+	SET_INP_FBK_9,
+	SET_INP_FBK_10,
+	SET_INP_FBK_11,
+	SET_INP_FBK_12,
+	SET_INP_FBK_13,
+	SET_INP_FBK_14,
+	SET_INP_FBK_15,
+	SET_INP_FBK_16,
+	SET_INP_FBK_17,
+	SET_INP_FBK_18,
+	SET_INP_FBK_19,
+	SET_INP_FBK_20,
+	SET_INP_FBK_21,
+	SET_INP_FBK_22,
+	SET_INP_FBK_23,
+	SET_INP_FBK_24,
+	SET_INP_FBK_25,
+	SET_INP_FBK_26,
+	SET_INP_FBK_27,
+	SET_INP_FBK_28,
+	SET_INP_FBK_29,
+	SET_INP_FBK_30,
+	SET_INP_FBK_31,
+	SET_INP_FBK_32,
+	SET_INP_FBK_33,
+	SET_INP_FBK_34,
+	SET_INP_FBK_35,
+	SET_INP_FBK_36,
+	SET_INP_FBK_37,
+	SET_INP_FBK_38,
+	SET_INP_FBK_39,
+	SET_INP_FBK_40,
+	SET_INP_FBK_41,
+	SET_INP_FBK_42,
+	SET_INP_FBK_43,
+	SET_INP_FBK_44,
+	SET_INP_FBK_45,
+	SET_INP_FBK_46,
+	SET_INP_FBK_47,
+	SET_INP_FBK_48,
+	SET_INP_FBK_49,
+	SET_INP_FBK_50,
+	SET_INP_FBK_51,
+	SET_INP_FBK_52,
+	SET_INP_FBK_53,
+	SET_INP_FBK_54,
+	SET_INP_FBK_55,
+	SET_INP_FBK_56,
+	SET_INP_FBK_57,
+	SET_INP_FBK_58,
+	SET_INP_FBK_59,
+	SET_INP_FBK_60,
+	SET_INP_FBK_61,
+	SET_INP_FBK_62,
+	SET_INP_FBK_63,
+	SET_INP_FBK_64,
+	SET_INP_FBK_65,
+	SET_INP_FBK_66,
+	SET_INP_FBK_67,
+	SET_INP_FBK_68,
+	SET_INP_FBK_69,
+	SET_INP_FBK_70,
+	SET_INP_FBK_71
 };
 enum jsc_element {
 	SET_JSC_PAD_A,
@@ -577,6 +652,12 @@ static const _opt opt_pad_type[] = {
 	{NULL, uL("auto")    , CTRL_PAD_AUTO},
 	{NULL, uL("original"), CTRL_PAD_ORIGINAL},
 	{NULL, uL("3rdparty"), CTRL_PAD_3RD_PARTY}
+};
+static const _opt opt_vk_size[] = {
+	{NULL, uL("1.0x")    , VK_SIZE_10X},
+	{NULL, uL("1.5x")    , VK_SIZE_15X},
+	{NULL, uL("2.0x")    , VK_SIZE_20X},
+	{NULL, uL("2.5x")    , VK_SIZE_25X}
 };
 
 static const _settings main_cfg[] = {
@@ -1417,7 +1498,103 @@ static const _settings inp_cfg[] = {
 		uL("# possible values: yes, no"),
 		NULL,
 		{LENGTH(opt_no_yes), opt_no_yes}
-	}
+	},
+	{
+		uL("virtual keyboard"), uL("size"), uL("1.0x"),
+		uL("# possible values: 1.0x, 1.5x, 2.0x, 2.5x"),
+		NULL,
+		{LENGTH(opt_vk_size), opt_vk_size}
+	},
+	// Row 0 - Column 1
+	{uL("family basic keyboard"), uL("FBK 0"),   uL("NSCODE_F8"),           NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 1"),   uL("NSCODE_Return"),       NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 2"),   uL("NSCODE_BrkRight"),     NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 3"),   uL("NSCODE_Bckslsh"),      NULL, NULL, {0, NULL}},
+	// Row 0 - Column 1
+	{uL("family basic keyboard"), uL("FBK 4"),   uL("NSCODE_CapsLck"),      NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 5"),   uL("NSCODE_RShift"),       NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 6"),   uL("NSCODE_Home"),         NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 7"),   uL("NSCODE_End"),          NULL, NULL, {0, NULL}},
+	// Row 1 - Column 0
+	{uL("family basic keyboard"), uL("FBK 8"),   uL("NSCODE_F7"),           NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 9"),   uL("NSCODE_BrkLeft"),      NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 10"),  uL("NSCODE_Apstrph"),      NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 11"),  uL("NSCODE_Semicln"),      NULL, NULL, {0, NULL}},
+	// Row 1 - Column 1
+	{uL("family basic keyboard"), uL("FBK 12"),  uL("NSCODE_QtLeft"),       NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 13"),  uL("NSCODE_Slash"),        NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 14"),  uL("NSCODE_Minus"),        NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 15"),  uL("NSCODE_Equal"),        NULL, NULL, {0, NULL}},
+	// Row 2 - Column 0
+	{uL("family basic keyboard"), uL("FBK 16"),  uL("NSCODE_F6"),           NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 17"),  uL("NSCODE_O"),            NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 18"),  uL("NSCODE_L"),            NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 19"),  uL("NSCODE_K"),            NULL, NULL, {0, NULL}},
+	// Row 2 - Column 1
+	{uL("family basic keyboard"), uL("FBK 20"),  uL("NSCODE_Period"),       NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 21"),  uL("NSCODE_Comma"),        NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 22"),  uL("NSCODE_P"),            NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 23"),  uL("NSCODE_0"),            NULL, NULL, {0, NULL}},
+	// Row 3 - Column 0
+	{uL("family basic keyboard"), uL("FBK 24"),  uL("NSCODE_F5"),           NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 25"),  uL("NSCODE_I"),            NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 26"),  uL("NSCODE_U"),            NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 27"),  uL("NSCODE_J"),            NULL, NULL, {0, NULL}},
+	// Row 3 - Column 1
+	{uL("family basic keyboard"), uL("FBK 28"),  uL("NSCODE_M"),            NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 29"),  uL("NSCODE_N"),            NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 30"),  uL("NSCODE_9"),            NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 31"),  uL("NSCODE_8"),            NULL, NULL, {0, NULL}},
+	// Row 4 - Column 0
+	{uL("family basic keyboard"), uL("FBK 32"),  uL("NSCODE_F4"),           NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 33"),  uL("NSCODE_Y"),            NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 34"),  uL("NSCODE_G"),            NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 35"),  uL("NSCODE_H"),            NULL, NULL, {0, NULL}},
+	// Row 4 - Column 1
+	{uL("family basic keyboard"), uL("FBK 36"),  uL("NSCODE_B"),            NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 37"),  uL("NSCODE_V"),            NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 38"),  uL("NSCODE_7"),            NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 39"),  uL("NSCODE_6"),            NULL, NULL, {0, NULL}},
+	// Row 5 - Column 0
+	{uL("family basic keyboard"), uL("FBK 40"),  uL("NSCODE_F3"),           NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 41"),  uL("NSCODE_T"),            NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 42"),  uL("NSCODE_R"),            NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 43"),  uL("NSCODE_D"),            NULL, NULL, {0, NULL}},
+	// Row 5 - Column 1
+	{uL("family basic keyboard"), uL("FBK 44"),  uL("NSCODE_F"),            NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 45"),  uL("NSCODE_C"),            NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 46"),  uL("NSCODE_5"),            NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 47"),  uL("NSCODE_4"),            NULL, NULL, {0, NULL}},
+	// Row 6 - Column 0
+	{uL("family basic keyboard"), uL("FBK 48"),  uL("NSCODE_F2"),           NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 49"),  uL("NSCODE_W"),            NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 50"),  uL("NSCODE_S"),            NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 51"),  uL("NSCODE_A"),            NULL, NULL, {0, NULL}},
+	// Row 6 - Column 1
+	{uL("family basic keyboard"), uL("FBK 52"),  uL("NSCODE_X"),            NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 53"),  uL("NSCODE_Z"),            NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 54"),  uL("NSCODE_E"),            NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 55"),  uL("NSCODE_3"),            NULL, NULL, {0, NULL}},
+	// Row 7 - Column 0
+	{uL("family basic keyboard"), uL("FBK 56"),  uL("NSCODE_F1"),           NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 57"),  uL("NSCODE_Escape"),       NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 58"),  uL("NSCODE_Q"),            NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 59"),  uL("NSCODE_LCtrl"),        NULL, NULL, {0, NULL}},
+	// Row 7 - Column 1
+	{uL("family basic keyboard"), uL("FBK 60"),  uL("NSCODE_LShift"),       NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 61"),  uL("NSCODE_Alt"),          NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 62"),  uL("NSCODE_1"),            NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 63"),  uL("NSCODE_2"),            NULL, NULL, {0, NULL}},
+	// Row 8 - Column 0
+	{uL("family basic keyboard"), uL("FBK 64"),  uL("NSCODE_Delete"),       NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 65"),  uL("NSCODE_Up"),           NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 66"),  uL("NSCODE_Right"),        NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 67"),  uL("NSCODE_Left"),         NULL, NULL, {0, NULL}},
+	// Row 8 - Column 1
+	{uL("family basic keyboard"), uL("FBK 68"),  uL("NSCODE_Down"),         NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 69"),  uL("NSCODE_Space"),        NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 70"),  uL("NSCODE_Bckspc"),       NULL, NULL, {0, NULL}},
+	{uL("family basic keyboard"), uL("FBK 71"),  uL("NSCODE_Insert"),       NULL, NULL, {0, NULL}}
 };
 
 static const _settings jsc_cfg[] = {
@@ -1482,6 +1659,9 @@ EXTERNC void settings_inp_wr_sc(void *str, int index, int type);
 EXTERNC void settings_inp_all_defaults(_config_input *config_input, _array_pointers_port *array);
 EXTERNC void settings_inp_port_defaults(_port *port, int index, int mode);
 EXTERNC void settings_inp_port_button_default(int button, _port *port, int index, int mode);
+EXTERNC DBWORD settings_inp_nes_keyboard_nscode_default(int element);
+EXTERNC DBWORD settings_inp_nes_keyboard_nscode(int element);
+EXTERNC void settings_inp_nes_keyboard_set_nscode(int element, DBWORD nscode);
 EXTERNC void settings_inp_save(void);
 
 EXTERNC void settings_pgs_parse(void);

src/gui/wdgSettingsInput.cpp
@@ -22,6 +22,7 @@
 #include "mainWindow.hpp"
 #include "dlgSettings.hpp"
 #include "dlgStdPad.hpp"
+#include "dlgKeyboard.hpp"
 #include "emu_thread.h"
 #include "conf.h"
 
@@ -638,6 +639,7 @@ void wdgSettingsInput::ports_end_misc_set_enabled(bool mode) {
 	comboBox_cp4->setEnabled(mode);
 	comboBox_exp->setEnabled(mode);
 
+	pushButton_ep->setEnabled(mode);
 	pushButton_cp1->setEnabled(mode);
 	pushButton_cp2->setEnabled(mode);
 	pushButton_cp3->setEnabled(mode);
@@ -708,6 +710,19 @@ void wdgSettingsInput::expansion_port_set(void) {
 			comboBox_exp->setEnabled(true);
 			break;
 	}
+
+	disconnect(pushButton_ep, SIGNAL(clicked(bool)), this, SLOT(s_expansion_port_setup(bool)));
+
+	switch (cfg->input.expansion) {
+		case CTRL_FAMILY_BASIC_KEYBOARD:
+			pushButton_ep->setEnabled(comboBox_exp->isEnabled());
+			//pb->setProperty("myPointer", QVariant::fromValue(((void *)ctrl_in)));
+			connect(pushButton_ep, SIGNAL(clicked(bool)), this, SLOT(s_expansion_port_setup(bool)));
+			break;
+		default:
+			pushButton_ep->setEnabled(false);
+			break;
+	}
 	icon_exp->setEnabled(comboBox_exp->isEnabled());
 	label_exp->setEnabled(comboBox_exp->isEnabled());
 }
@@ -742,13 +757,11 @@ void wdgSettingsInput::controller_ports_set(void) {
 			case CTRL_ZAPPER:
 			case CTRL_SNES_MOUSE:
 			case CTRL_ARKANOID_PADDLE:
-			case CTRL_FAMILY_BASIC_KEYBOARD:
 			default:
 				pb->setEnabled(false);
 				break;
 			case CTRL_STANDARD:
 				pb->setEnabled(true);
-				pb->setProperty("myPointer", QVariant::fromValue(((void *)ctrl_in)));
 				connect(pb, SIGNAL(clicked(bool)), this, SLOT(s_controller_port_setup(bool)));
 				break;
 		}
@@ -863,6 +876,19 @@ void wdgSettingsInput::s_expansion_port(int index) {
 	input_init(SET_CURSOR);
 	emu_thread_continue();
 	update_widget();
+}
+void wdgSettingsInput::s_expansion_port_setup(UNUSED(bool checked)) {
+	switch (cfg->input.expansion) {
+		case CTRL_FAMILY_BASIC_KEYBOARD:
+			if (dlgkeyb->isHidden()) {
+				mainwin->open_dkeyb(dlgKeyboard::DK_SETUP);
+			} else {
+				dlgkeyb->switch_mode(dlgKeyboard::DK_SETUP);
+			}
+			break;
+		default:
+			break;
+	}
 }
 void wdgSettingsInput::s_controller_port(int index) {
 	QList<QVariant> type = ((QComboBox *)sender())->itemData(index).toList();

configure.ac
@@ -189,7 +189,7 @@ AS_IF([test "x$PKG_CONFIG" = "xno"], [
 # Checks for header files.
 AC_CHECK_HEADERS([fcntl.h float.h inttypes.h limits.h stddef.h stdint.h stdlib.h string.h strings.h sys/ioctl.h sys/time.h unistd.h utime.h])
 
-AX_CXX_COMPILE_STDCXX_14([noext])
+AX_CXX_COMPILE_STDCXX_17([noext])
 
 OLD_CFLAGS="$CFLAGS"
 OLD_LIBS="$LIBS"

src/core/bck_states.c
@@ -181,12 +181,12 @@ void bck_states_op_input(BYTE mode, void *data, size_t *index, size_t *size_buff
 	bck_states_on_struct(mode, gmouse.left, data, (*index), (*size_buff));
 	bck_states_on_struct(mode, gmouse.right, data, (*index), (*size_buff));
 
-	// family basic keyboard
-	bck_states_on_struct(mode, family_basic_keyboard.row, data, (*index), (*size_buff));
-	bck_states_on_struct(mode, family_basic_keyboard.column, data, (*index), (*size_buff));
-	bck_states_on_struct(mode, family_basic_keyboard.enable, data, (*index), (*size_buff));
-	bck_states_on_struct(mode, family_basic_keyboard.state, data, (*index), (*size_buff));
-	bck_states_on_struct(mode, family_basic_keyboard.data, data, (*index), (*size_buff));
+	// generic keyboard
+	bck_states_on_struct(mode, generic_keyboard.row, data, (*index), (*size_buff));
+	bck_states_on_struct(mode, generic_keyboard.column, data, (*index), (*size_buff));
+	bck_states_on_struct(mode, generic_keyboard.enable, data, (*index), (*size_buff));
+	bck_states_on_struct(mode, generic_keyboard.state, data, (*index), (*size_buff));
+	bck_states_on_struct(mode, generic_keyboard.data, data, (*index), (*size_buff));
 
 	// lag frames
 	bck_states_on_struct(mode, tas.total_lag_frames, data, (*index), (*size_buff));

src/core/conf.h
@@ -93,6 +93,7 @@ typedef struct _config {
 	BYTE nsf_player_effect;
 	BYTE nsf_player_nsfe_playlist;
 	BYTE nsf_player_nsfe_fadeout;
+	BYTE vk_size;
 #if defined (FULLSCREEN_RESFREQ)
 	BYTE adaptive_rrate;
 	int fullscreen_res_w;

src/core/input.c
@@ -37,6 +37,8 @@
 #include "input/oeka_kids_tablet.h"
 #include "input/family_basic_keyboard.h"
 
+INLINE static void input_init_generic_keyboard(void);
+
 #define SET_WR_REG(funct) input_wr_reg = funct
 #define SET_RD_REG(id, funct) input_rd_reg[id] = funct
 
@@ -50,6 +52,7 @@ _r4016 r4016;
 _port port[PORT_MAX];
 _port_funct port_funct[PORT_MAX];
 _nes_keyboard nes_keyboard;
+_generic_keyboard generic_keyboard;
 _mic mic;
 
 BYTE (*input_wr_reg)(BYTE value);
@@ -68,7 +71,7 @@ void input_init(BYTE set_cursor) {
 	input_init_arkanoid();
 	input_init_oeka_kids_tablet();
 	input_init_nsf_mouse();
-	input_init_family_basic_keyboard();
+	input_init_generic_keyboard();
 
 	memset(&mic, 0x00, sizeof(mic));
 
@@ -301,4 +304,8 @@ BYTE input_draw_target(void) {
 	}
 
 	return (FALSE);
+}
+
+INLINE static void input_init_generic_keyboard(void) {
+        memset(&generic_keyboard, 0x00, sizeof(generic_keyboard));
 }

src/core/input.h
@@ -37,7 +37,7 @@ enum controller_types {
 	CTRL_SNES_MOUSE,
 	CTRL_ARKANOID_PADDLE,
 	CTRL_OEKA_KIDS_TABLET,
-	CTRL_FAMILY_BASIC_KEYBOARD,
+	CTRL_FAMILY_BASIC_KEYBOARD
 };
 enum pad_types { CTRL_PAD_AUTO, CTRL_PAD_ORIGINAL, CTRL_PAD_3RD_PARTY };
 enum controller_buttons {
@@ -68,6 +68,7 @@ enum port_controllers {
 };
 enum nes_keyboard_misc { NES_KEYBOARD_MAX_KEYS = 105 };
 enum mic_modes { MIC_NONE, MIC_RESET, MIC_STOP };
+enum vk_size { VK_SIZE_10X, VK_SIZE_15X, VK_SIZE_20X, VK_SIZE_25X };
 
 #if defined (_WIN32)
 typedef GUID _input_guid;
@@ -109,13 +110,13 @@ typedef struct _port {
 typedef struct _arkanoid {
 	int x, rdx, button;
 } _arkanoid;
-typedef struct _family_basic_keyboard {
+typedef struct _generic_keyboard {
 	BYTE row;
 	BYTE column;
 	BYTE enable;
 	BYTE state;
-	BYTE data[10];
-} _family_basic_keyboard;
+	BYTE data[13];
+} _generic_keyboard;
 typedef struct _mic {
 	BYTE enable;
 	BYTE mode;
@@ -131,20 +132,13 @@ typedef struct _port_funct {
 	void (*input_add_event)(BYTE index);
 	BYTE (*input_decode_event)(BYTE mode, BYTE autorepeat, DBWORD event, BYTE type, _port *port);
 } _port_funct;
-typedef struct _nes_keyboard_keycode {
-	DBWORD code;
-	BYTE row;
-	BYTE column;
-	WORD index;
-} _nes_keyboard_keycode;
 typedef struct _nes_keyboard {
 	BYTE enabled;
 	SBYTE type;
 	WORD rows;
 	WORD columns;
 	WORD totals;
-	BYTE keys[NES_KEYBOARD_MAX_KEYS];
-	_nes_keyboard_keycode keycodes[NES_KEYBOARD_MAX_KEYS];
+	BYTE matrix[NES_KEYBOARD_MAX_KEYS];
 } _nes_keyboard;
 
 extern _r4016 r4016;
@@ -152,7 +146,7 @@ extern _port port[PORT_MAX];
 extern _port_funct port_funct[PORT_MAX];
 extern _arkanoid arkanoid[PORT_BASE];
 extern _nes_keyboard nes_keyboard;
-extern _family_basic_keyboard family_basic_keyboard;
+extern _generic_keyboard generic_keyboard;
 extern _mic mic;
 
 extern BYTE (*input_wr_reg)(BYTE value);

src/core/input/family_basic_keyboard.c
@@ -23,12 +23,6 @@
 #include "conf.h"
 #include "gui.h"
 
-_family_basic_keyboard family_basic_keyboard;
-
-void input_init_family_basic_keyboard(void) {
-	memset(&family_basic_keyboard, 0x00, sizeof(family_basic_keyboard));
-}
-
 void input_wr_family_basic_keyboard(BYTE *value, UNUSED(BYTE nport)) {
 	BYTE column;
 
@@ -39,16 +33,16 @@ void input_wr_family_basic_keyboard(BYTE *value, UNUSED(BYTE nport)) {
 	//       ||+-- Reset the keyboard to the first row.
 	//       |+--- Select column, row is incremented if this bit goes from high to low.
 	//       +---- Enable keyboard matrix (if 0, all voltages inside the keyboard will be 5V, reading back as logical 0 always)
-	family_basic_keyboard.enable = (*value) & 0x04;
+	generic_keyboard.enable = (*value) & 0x04;
 	column = ((*value) & 0x02) >> 1;
-	if (family_basic_keyboard.enable) {
+	if (generic_keyboard.enable) {
 		if ((*value) & 0x01) {
-			family_basic_keyboard.row = 0;
+			generic_keyboard.row = 0;
 			gui_nes_keyboard_paste_event();
-		} else if (family_basic_keyboard.column && !column) {
-			family_basic_keyboard.row = (family_basic_keyboard.row + 1) % 10;
+		} else if (generic_keyboard.column && !column) {
+			generic_keyboard.row = (generic_keyboard.row + 1) % 10;
 		}
-		family_basic_keyboard.column = column;
+		generic_keyboard.column = column;
 	}
 
 	// tape data recorder
@@ -70,12 +64,12 @@ void input_rd_family_basic_keyboard(BYTE *value, BYTE nport, UNUSED(BYTE shift))
 		// xxxK KKKx
 		//    | |||
 		//    +-+++--- Receive key status of currently selected row/column.
-		if (family_basic_keyboard.column) {
-			state = (family_basic_keyboard.data[family_basic_keyboard.row] & 0xF0) >> 3;
+		if (generic_keyboard.column) {
+			state = (generic_keyboard.data[generic_keyboard.row] & 0xF0) >> 3;
 		} else {
-			state = (family_basic_keyboard.data[family_basic_keyboard.row] & 0x0F) << 1;
+			state = (generic_keyboard.data[generic_keyboard.row] & 0x0F) << 1;
 		}
-		(*value) = ((*value) & 0xE1) | (family_basic_keyboard.enable ? state ^ 0x1E : 0x00);
+		(*value) = ((*value) & 0xE1) | (generic_keyboard.enable ? state ^ 0x1E : 0x00);
 	} else {
 		// r4016
 		// 7  bit  0
@@ -83,7 +77,7 @@ void input_rd_family_basic_keyboard(BYTE *value, BYTE nport, UNUSED(BYTE shift))
 		// xxxx xxAx
 		//        |
 		//        +-- 1-bit ADC audio from audio cassette
-		(*value) = ((*value) & 0xFD) | ((r4016.value & 0x04) ? ((tape_data_recorder.out & 0x01) << 1) : 0x00);
+		(*value) = ((*value) & 0xFD) | (generic_keyboard.enable ? ((tape_data_recorder.out & 0x01) << 1) : 0x00);
 	}
 }
 
@@ -91,10 +85,10 @@ void input_add_event_family_basic_keyboard(UNUSED(BYTE index)) {
 	WORD a, b;
 
 	for (a = 0; a < nes_keyboard.rows; a++) {
-		family_basic_keyboard.data[a] = 0;
+		generic_keyboard.data[a] = 0;
 		for (b = 0; b < nes_keyboard.columns; b++) {
-			if (nes_keyboard.keys[(a * nes_keyboard.columns) + b] & 0x80) {
-				family_basic_keyboard.data[a] |= (1 << b);
+			if (nes_keyboard.matrix[(a * nes_keyboard.columns) + b] & 0x80) {
+				generic_keyboard.data[a] |= (1 << b);
 			}
 		}
 	}

commit cb71f00207af3eb06a7556f3a11585d70ffce974

Fixed regression inroduced with https://www.codefactor.io/repository/github/punesemu/punes/commit/3c7b77be71c12792ea6cc0b02f7c280a38e74c79/master.
I deleted the wrong line.

src/gui/settings.h
@@ -455,8 +455,6 @@ enum inp_element {
 	SET_INP_SBKB_98,
 	SET_INP_SBKB_99,
 	SET_INP_SBKB_END = SET_INP_SBKB_99,
-
-
 };
 enum jsc_element {
 	SET_JSC_PAD_A,

src/gui/wdgSettingsInput.cpp
@@ -718,7 +718,6 @@ void wdgSettingsInput::expansion_port_set(void) {
 		case CTRL_FAMILY_BASIC_KEYBOARD:
 		case CTRL_SUBOR_KEYBOARD:
 			pushButton_ep->setEnabled(comboBox_exp->isEnabled());
-			//pb->setProperty("myPointer", QVariant::fromValue(((void *)ctrl_in)));
 			connect(pushButton_ep, SIGNAL(clicked(bool)), this, SLOT(s_expansion_port_setup(bool)));
 			break;
 		default:
@@ -764,6 +763,7 @@ void wdgSettingsInput::controller_ports_set(void) {
 				break;
 			case CTRL_STANDARD:
 				pb->setEnabled(true);
+				pb->setProperty("myPointer", QVariant::fromValue(((void *)ctrl_in)));
 				connect(pb, SIGNAL(clicked(bool)), this, SLOT(s_controller_port_setup(bool)));
 				break;
 		}