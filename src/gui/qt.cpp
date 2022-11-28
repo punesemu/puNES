punesemu/puNES

commit f6f2a9d6b4ad9d7aa30d7abf6a36adfe2629e3f3

Added a Wizard to select the operating mode the first time the emulator is run.

src/gui/qt.cpp
@@ -73,6 +73,7 @@ Q_IMPORT_PLUGIN(QSvgPlugin)
 #if defined (WITH_D3D9)
 #include "d3d9.h"
 #endif
+#include "cmd_line.h"
 
 INLINE void gui_init_os(void);
 INLINE uTCHAR *gui_home(void);
@@ -137,6 +138,8 @@ BYTE gui_init(int *argc, char **argv) {
 	memset(&gui, 0, sizeof(gui));
 	qt = {};
 	qt.app = new mainApplication((*argc), argv, true, mode);
+
+	info.portable = cmd_line_check_portable((*argc), (uTCHAR **)argv);
 
 	info.gui = TRUE;
 	gui.in_update = FALSE;
@@ -456,6 +459,7 @@ int gui_uncompress_selection_dialog(_uncompress_archive *archive, BYTE type) {
 
 	dlg->show();
 	dlg->exec();
+	delete(dlg);
 
 	return (gui.dlg_rc);
 }

src/CMakeLists.txt
@@ -455,6 +455,7 @@ set(gui_ui_srcs
 	gui/designer/dlgStdPad.ui
 	gui/designer/dlgUncomp.ui
 	gui/designer/dlgVsSystem.ui
+	gui/designer/dlgWizard.ui
 	gui/designer/mainWindow.ui
 	gui/designer/wdgAPUChannels.ui
 	gui/designer/wdgCheatsEditor.ui
@@ -494,6 +495,8 @@ set(gui_srcs
 	gui/dlgUncomp.hpp
 	gui/dlgVsSystem.cpp
 	gui/dlgVsSystem.hpp
+	gui/dlgWizard.cpp
+	gui/dlgWizard.hpp
 	gui/mainApplication.cpp
 	gui/mainApplication.hpp
 	gui/mainWindow.cpp

src/core/main.c
@@ -57,12 +57,6 @@ int main(int argc, char **argv) {
 	info.doublebuffer = TRUE;
 	info.machine[HEADER] = info.machine[DATABASE] = DEFAULT;
 
-	if (cmd_line_check_portable(argc, argv) == TRUE) {
-		info.portable = TRUE;
-	} else {
-		info.portable = FALSE;
-	}
-
 	if (gui_init(&argc, (char **)argv) == EXIT_ERROR) {
 		return (EXIT_ERROR);
 	}

src/gui/cmd_line.cpp
@@ -296,11 +296,11 @@ BYTE cmd_line_parse(int argc, uTCHAR **argv) {
 				break;
 		}
 	}
-
 	return (EXIT_OK);
 }
 BYTE cmd_line_check_portable(int argc, uTCHAR **argv) {
-	if (QFileInfo(uQString(argv[0])).completeBaseName().right(2) == "_p") {
+	if ((QFileInfo(uQString(argv[0])).completeBaseName().right(2) == "_p") ||
+		(QFile(uQString(gui_application_folder()) + QString(CFGFILENAME)).exists())) {
 		return (TRUE);
 	}
 	for (int opt = 0; opt < argc; opt++) {
@@ -314,8 +314,9 @@ BYTE cmd_line_check_portable(int argc, uTCHAR **argv) {
 }
 
 static void usage(QString &name) {
-	dlgCmdLineHelp *box = new dlgCmdLineHelp(nullptr, name);
+	dlgCmdLineHelp *dlg = new dlgCmdLineHelp(nullptr, name);
 
-	box->show();
-	box->exec();
+	dlg->show();
+	dlg->exec();
+	delete(dlg);
 }

src/gui/designer/dlgWizard.ui
@@ -1,0 +1,236 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>dlgWizard</class>
+ <widget class="QDialog" name="dlgWizard">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>454</width>
+    <height>235</height>
+   </rect>
+  </property>
+  <property name="sizePolicy">
+   <sizepolicy hsizetype="Minimum" vsizetype="Preferred">
+    <horstretch>0</horstretch>
+    <verstretch>0</verstretch>
+   </sizepolicy>
+  </property>
+  <property name="minimumSize">
+   <size>
+    <width>0</width>
+    <height>235</height>
+   </size>
+  </property>
+  <property name="windowTitle">
+   <string notr="true">Wizard</string>
+  </property>
+  <property name="windowIcon">
+   <iconset resource="resources.qrc">
+    <normaloff>:/icon/icons/preferences_other.svg</normaloff>:/icon/icons/preferences_other.svg</iconset>
+  </property>
+  <layout class="QVBoxLayout" name="verticalLayout_dlgWizard">
+   <item>
+    <widget class="QLabel" name="label_Title">
+     <property name="font">
+      <font>
+       <pointsize>21</pointsize>
+       <weight>75</weight>
+       <bold>true</bold>
+      </font>
+     </property>
+     <property name="text">
+      <string notr="true">puNES Initial Setup</string>
+     </property>
+     <property name="scaledContents">
+      <bool>true</bool>
+     </property>
+     <property name="alignment">
+      <set>Qt::AlignCenter</set>
+     </property>
+    </widget>
+   </item>
+   <item>
+    <spacer name="verticalSpacer">
+     <property name="orientation">
+      <enum>Qt::Vertical</enum>
+     </property>
+     <property name="sizeType">
+      <enum>QSizePolicy::Fixed</enum>
+     </property>
+     <property name="sizeHint" stdset="0">
+      <size>
+       <width>20</width>
+       <height>10</height>
+      </size>
+     </property>
+    </spacer>
+   </item>
+   <item>
+    <layout class="QGridLayout" name="gridLayout_Data_Storage">
+     <item row="4" column="3">
+      <widget class="QLabel" name="label_Folder">
+       <property name="sizePolicy">
+        <sizepolicy hsizetype="Expanding" vsizetype="Preferred">
+         <horstretch>0</horstretch>
+         <verstretch>0</verstretch>
+        </sizepolicy>
+       </property>
+       <property name="text">
+        <string notr="true">[folder]</string>
+       </property>
+      </widget>
+     </item>
+     <item row="2" column="0" colspan="4">
+      <widget class="QRadioButton" name="radioButton_Data_Storage_User">
+       <property name="text">
+        <string notr="true">User profile location</string>
+       </property>
+      </widget>
+     </item>
+     <item row="4" column="0">
+      <widget class="QLabel" name="label">
+       <property name="maximumSize">
+        <size>
+         <width>16</width>
+         <height>16</height>
+        </size>
+       </property>
+       <property name="text">
+        <string notr="true"/>
+       </property>
+       <property name="pixmap">
+        <pixmap resource="resources.qrc">:/icon/icons/open.svg</pixmap>
+       </property>
+       <property name="scaledContents">
+        <bool>true</bool>
+       </property>
+      </widget>
+     </item>
+     <item row="4" column="1" colspan="2">
+      <widget class="QLabel" name="label_Desc_Folder">
+       <property name="font">
+        <font>
+         <weight>50</weight>
+         <italic>true</italic>
+         <bold>false</bold>
+        </font>
+       </property>
+       <property name="text">
+        <string notr="true">Folder :</string>
+       </property>
+       <property name="scaledContents">
+        <bool>true</bool>
+       </property>
+      </widget>
+     </item>
+     <item row="0" column="0" colspan="4">
+      <layout class="QHBoxLayout" name="horizontalLayout_Data_Storage">
+       <item>
+        <widget class="QLabel" name="icon_Data_Storage">
+         <property name="maximumSize">
+          <size>
+           <width>16</width>
+           <height>16</height>
+          </size>
+         </property>
+         <property name="text">
+          <string notr="true"/>
+         </property>
+         <property name="pixmap">
+          <pixmap resource="resources.qrc">:/icon/icons/home.svg</pixmap>
+         </property>
+         <property name="scaledContents">
+          <bool>true</bool>
+         </property>
+        </widget>
+       </item>
+       <item>
+        <widget class="QLabel" name="label_Data_Storage">
+         <property name="font">
+          <font>
+           <weight>75</weight>
+           <bold>true</bold>
+          </font>
+         </property>
+         <property name="text">
+          <string notr="true">Data Storage Folder :</string>
+         </property>
+        </widget>
+       </item>
+       <item>
+        <widget class="Line" name="line_Data_Storage">
+         <property name="sizePolicy">
+          <sizepolicy hsizetype="Expanding" vsizetype="Fixed">
+           <horstretch>0</horstretch>
+           <verstretch>0</verstretch>
+          </sizepolicy>
+         </property>
+         <property name="orientation">
+          <enum>Qt::Horizontal</enum>
+         </property>
+        </widget>
+       </item>
+      </layout>
+     </item>
+     <item row="1" column="0" colspan="4">
+      <widget class="QRadioButton" name="radioButton_Data_Storage_Portable">
+       <property name="text">
+        <string notr="true">Same folder as the executable</string>
+       </property>
+      </widget>
+     </item>
+    </layout>
+   </item>
+   <item>
+    <layout class="QHBoxLayout" name="horizontalLayout_line">
+     <item>
+      <widget class="Line" name="line">
+       <property name="sizePolicy">
+        <sizepolicy hsizetype="Expanding" vsizetype="Fixed">
+         <horstretch>0</horstretch>
+         <verstretch>0</verstretch>
+        </sizepolicy>
+       </property>
+       <property name="minimumSize">
+        <size>
+         <width>0</width>
+         <height>20</height>
+        </size>
+       </property>
+       <property name="orientation">
+        <enum>Qt::Horizontal</enum>
+       </property>
+      </widget>
+     </item>
+    </layout>
+   </item>
+   <item>
+    <widget class="QPushButton" name="pushButton_Start">
+     <property name="sizePolicy">
+      <sizepolicy hsizetype="Minimum" vsizetype="Expanding">
+       <horstretch>0</horstretch>
+       <verstretch>0</verstretch>
+      </sizepolicy>
+     </property>
+     <property name="text">
+      <string notr="true">Start</string>
+     </property>
+     <property name="icon">
+      <iconset resource="resources.qrc">
+       <normaloff>:/icon/icons/confirm.svg</normaloff>:/icon/icons/confirm.svg</iconset>
+     </property>
+    </widget>
+   </item>
+  </layout>
+ </widget>
+ <tabstops>
+  <tabstop>pushButton_Start</tabstop>
+  <tabstop>radioButton_Data_Storage_User</tabstop>
+ </tabstops>
+ <resources>
+  <include location="resources.qrc"/>
+  <include location="resources.qrc"/>
+ </resources>
+ <connections/>
+</ui>

src/gui/dlgWizard.cpp
@@ -1,0 +1,65 @@
+/*
+ *  Copyright (C) 2010-2022 Fabio Cavallo (aka FHorse)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include "dlgWizard.hpp"
+#include "info.h"
+
+dlgWizard::dlgWizard(QWidget *parent, const QString &config_folder, const QString &application_folder) : QDialog(parent) {
+	setupUi(this);
+
+	cfg_folder = config_folder;
+	app_folder = application_folder;
+
+	{
+		grp = new QButtonGroup(this);
+
+		grp->addButton(radioButton_Data_Storage_Portable);
+		grp->setId(radioButton_Data_Storage_Portable, 0);
+		grp->addButton(radioButton_Data_Storage_User);
+		grp->setId(radioButton_Data_Storage_User, 1);
+
+		connect(grp, SIGNAL(buttonClicked(QAbstractButton*)), this, SLOT(s_grp_storage_type(QAbstractButton*)));
+	}
+	connect(pushButton_Start, SIGNAL(clicked(bool)), this, SLOT(s_accepted(bool)));
+}
+dlgWizard::~dlgWizard() = default;
+
+void dlgWizard::showEvent(UNUSED(QShowEvent *event)) {
+	radioButton_Data_Storage_Portable->click();
+	pushButton_Start->setFocus();
+}
+
+void dlgWizard::s_grp_storage_type(UNUSED(QAbstractButton *button)) {
+	QString folder;
+
+	switch (grp->checkedId()) {
+		default:
+		case 0:
+			folder = app_folder;
+			info.portable = TRUE;
+			break;
+		case 1:
+			folder = cfg_folder;
+			info.portable = FALSE;
+			break;
+	}
+	label_Folder->setText(folder);
+}
+void dlgWizard::s_accepted(UNUSED(bool checked)) {
+	accept();
+}

src/gui/dlgWizard.hpp
@@ -1,0 +1,47 @@
+/*
+ *  Copyright (C) 2010-2022 Fabio Cavallo (aka FHorse)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef DLGWIZARD_HPP_
+#define DLGWIZARD_HPP_
+
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QButtonGroup>
+#include "ui_dlgWizard.h"
+#include "common.h"
+
+class dlgWizard : public QDialog, public Ui::dlgWizard {
+	Q_OBJECT
+
+	private:
+		QButtonGroup *grp;
+		QString cfg_folder;
+		QString app_folder;
+
+	public:
+		explicit dlgWizard(QWidget *parent = nullptr, const QString &config_folder = "", const QString &application_folder = "");
+		~dlgWizard() override;
+
+	private:
+		void showEvent(QShowEvent *event) override;
+
+	private slots:
+		void s_grp_storage_type(QAbstractButton *button);
+		void s_accepted(bool checked);
+};
+
+#endif /* DLGWIZARD_HPP_ */

src/gui/mainApplication.cpp
@@ -22,20 +22,21 @@
 #include <QtGui/QKeySequence>
 #include "mainApplication.hpp"
 #include "mainWindow.hpp"
+#include "dlgWizard.hpp"
 #include "dlgKeyboard.hpp"
 #include "version.h"
 #include "gui.h"
 
-#define FONT_SIZE 9
+//#define FONT_SIZE 9
 
 mainApplication::mainApplication(int &argc, char *argv[], bool allowSecondary, Options options, int timeout, const QString &userData) :
 	SingleApplication(argc, argv, allowSecondary, options, timeout, userData) {
 	QFont f = font();
 
-	if (f.pointSize() != FONT_SIZE) {
+	//if (f.pointSize() != FONT_SIZE) {
 	//	f.setPointSize(FONT_SIZE);
 	//	setFont(f);
-	}
+	//}
 }
 mainApplication::~mainApplication() = default;
 
@@ -86,6 +87,20 @@ BYTE mainApplication::control_base_folders(void) {
 #else
 	QDir old(QString("%0/.%1").arg(uQString(gui_home_folder()), NAME));
 #endif
+
+	if (!info.portable && !old.exists() && !config_folder.exists()) {
+		dlgWizard *dlg = new dlgWizard(nullptr, config_folder.absolutePath(), uQString(gui_application_folder()));
+
+		dlg->show();
+		if (dlg->exec() == QDialog::Rejected) {
+			delete (dlg);
+			return (EXIT_ERROR);
+		}
+		delete (dlg);
+		config_folder.setPath(uQString(gui_config_folder()));
+		data_folder.setPath(uQString(gui_data_folder()));
+		temp_folder.setPath(uQString(gui_temp_folder()));
+	}
 
 	// controllo l'esistenza della directory principale
 	if (base_folder(&config_folder, nullptr, ".", tr("Error on create config folder")) == EXIT_ERROR) {
@@ -140,7 +155,6 @@ BYTE mainApplication::control_base_folders(void) {
 		}
 		old.removeRecursively();
 	}
-
 	return (EXIT_OK);
 }
 
@@ -158,7 +172,7 @@ QKeySequence mainApplication::key_sequence_from_key_event(QKeyEvent *event) {
 	return (QKeySequence(modifiers ? (int)modifiers : key, modifiers ? key : 0).toString().remove(", "));
 }
 bool mainApplication::is_set_inp_shortcut(QEvent *event, int set_inp) {
-	return (!mainwin->shortcut[set_inp]->key().isEmpty() &&
+	return (mainwin && !mainwin->shortcut[set_inp]->key().isEmpty() &&
 		(key_sequence_from_key_event((QKeyEvent *)event) == mainwin->shortcut[set_inp]->key()));
 }
 bool mainApplication::dlgkeyb_event(QEvent *event) {

commit 5c7322d50971c8b239da863cc905838948015fb2

Fixed crash introduced with https://github.com/punesemu/puNES/commit/f6f2a9d6b4ad9d7aa30d7abf6a36adfe2629e3f3.

src/gui/qt.cpp
@@ -459,7 +459,6 @@ int gui_uncompress_selection_dialog(_uncompress_archive *archive, BYTE type) {
 
 	dlg->show();
 	dlg->exec();
-	delete(dlg);
 
 	return (gui.dlg_rc);
 }