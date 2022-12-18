5e67cc5e9179012c0093826254d2bc8af666e164
Fabio Cavallo (FHorse) <punes.development@gmail.com>
2022-12-18 10:26:13 +0100

    New translations en_EN.ts (Hungarian)

--- src/gui/designer/translations/hu_HU.ts
+++ src/gui/designer/translations/hu_HU.ts
@@ -1489,10 +1489,6 @@
       <translation type="unfinished">Toggle Virtual &amp;Keyboard</translation>
     </message>
     <message>
-      <source>Start FDS Emulation &amp;without Disk</source>
-      <translation type="unfinished">Start FDS Emulation &amp;without Disk</translation>
-    </message>
-    <message>
       <source>&amp;Show Log</source>
       <translation type="unfinished">&amp;Show Log</translation>
     </message>
@@ -2461,16 +2457,16 @@
       <translation>Famicom Disk System</translation>
     </message>
     <message>
-      <source>Use fast forward when loading the BIOS or disk/side</source>
-      <translation>Gyors előretekerés használata, amikor a BIOS vagy lemezoldal töltődik</translation>
-    </message>
-    <message>
       <source>Switch the disk/side automatically</source>
       <translation>Lemezoldalak autováltása</translation>
     </message>
     <message>
       <source>Allow multiple instances of the emulator</source>
       <translation>Több példány engedélyezése az emulátorból</translation>
+    </message>
+    <message>
+      <source>Use fast forward when loading the disk/side</source>
+      <translation type="unfinished">Use fast forward when loading the disk/side</translation>
     </message>
   </context>
   <context>
