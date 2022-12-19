08ce9d25319f8f9c50ce34bba376f84a78a0f4be
Fabio Cavallo <punes.development@gmail.com>
2022-12-19 01:11:28 +0100

    Update en_EN.ts.

--- src/gui/designer/translations/en_EN.ts
+++ src/gui/designer/translations/en_EN.ts
@@ -2468,6 +2468,10 @@
         <source>Use fast forward when loading the disk/side</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>French</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>wdgSettingsInput</name>
