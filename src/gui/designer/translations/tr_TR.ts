70921cd5acc6e86892e4567d4a1ab90787ae8de2
Fabio Cavallo (FHorse) <punes.development@gmail.com>
2022-12-29 16:18:15 +0100

    New translations en_EN.ts (Turkish)

--- src/gui/designer/translations/tr_TR.ts
+++ src/gui/designer/translations/tr_TR.ts
@@ -1966,11 +1966,11 @@
     </message>
     <message>
       <source>VBlend</source>
-      <translation type="unfinished">VBlend</translation>
+      <translation>VBlend</translation>
     </message>
     <message>
       <source>Brightness</source>
-      <translation type="unfinished">Brightness</translation>
+      <translation>Parlaklık</translation>
     </message>
     <message>
       <source>IWidth</source>
@@ -1978,31 +1978,27 @@
     </message>
     <message>
       <source>Scanline</source>
-      <translation type="unfinished">Scanline</translation>
+      <translation>Scanline</translation>
     </message>
     <message>
       <source>Saturation</source>
-      <translation type="unfinished">Saturation</translation>
+      <translation>Doygunluk</translation>
     </message>
     <message>
       <source>Contrast</source>
-      <translation type="unfinished">Contrast</translation>
+      <translation>Kontrast</translation>
     </message>
     <message>
       <source>Hue</source>
-      <translation type="unfinished">Hue</translation>
+      <translation>Hue</translation>
     </message>
     <message>
-      <source>Reset paramaters to defaults</source>
-      <translation type="unfinished">Reset paramaters to defaults</translation>
+      <source>Reset parameters to defaults</source>
+      <translation type="unfinished">Reset parameters to defaults</translation>
     </message>
   </context>
   <context>
     <name>wdgNTSCFilter</name>
-    <message>
-      <source>Reset paramaters to defaults</source>
-      <translation>Parametreleri varsayılana sıfırla</translation>
-    </message>
     <message>
       <source>Artifacts</source>
       <translation>Yapılar</translation>
@@ -2054,6 +2050,10 @@
     <message>
       <source>Scanline</source>
       <translation>Scanline</translation>
+    </message>
+    <message>
+      <source>Reset parameters to defaults</source>
+      <translation type="unfinished">Reset parameters to defaults</translation>
     </message>
   </context>
   <context>
@@ -3199,10 +3199,6 @@
       <translation>NTSC RGB</translation>
     </message>
     <message>
-      <source>Reset paramaters to defaults</source>
-      <translation>Parametreleri varsayılana sıfırla</translation>
-    </message>
-    <message>
       <source>Filters</source>
       <translation>Filtreler</translation>
     </message>
@@ -3461,6 +3457,10 @@
     <message>
       <source>NTSC Bisqwit 8X</source>
       <translation type="unfinished">NTSC Bisqwit 8X</translation>
+    </message>
+    <message>
+      <source>Reset parameters to defaults</source>
+      <translation type="unfinished">Reset parameters to defaults</translation>
     </message>
   </context>
   <context>
