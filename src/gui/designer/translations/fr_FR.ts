8bc7c4e82545cbe916a41e3df6b850ca2fa2bf62
Benjamin Siskoo <siskoo@tradu-france.com>
2022-12-29 16:49:17 +0100

    Updated frenc language by Benjamin Siskoo

--- src/gui/designer/translations/fr_FR.ts
+++ src/gui/designer/translations/fr_FR.ts
@@ -1958,11 +1958,11 @@
     <name>wdgNTSCBisqwitFilter</name>
     <message>
       <source>QWidth</source>
-      <translation type="unfinished">QWidth</translation>
+      <translation type="unfinished">LargeurQ</translation>
     </message>
     <message>
       <source>YWidth</source>
-      <translation type="unfinished">YWidth</translation>
+      <translation type="unfinished">LargeurY</translation>
     </message>
     <message>
       <source>VBlend</source>
@@ -1974,7 +1974,7 @@
     </message>
     <message>
       <source>IWidth</source>
-      <translation type="unfinished">IWidth</translation>
+      <translation type="unfinished">LargeurI</translation>
     </message>
     <message>
       <source>Scanline</source>
@@ -1994,7 +1994,7 @@
     </message>
     <message>
       <source>Reset parameters to defaults</source>
-      <translation>Rsémarrer les paramètres par défaut</translation>
+      <translation>Redémarrer les paramètres par défaut</translation>
     </message>
   </context>
   <context>
@@ -2053,7 +2053,7 @@
     </message>
     <message>
       <source>Reset parameters to defaults</source>
-      <translation>Rsémarrer les paramètres par défaut</translation>
+      <translation>Redémarrer les paramètres par défaut</translation>
     </message>
   </context>
   <context>
@@ -3460,7 +3460,7 @@
     </message>
     <message>
       <source>Reset parameters to defaults</source>
-      <translation>Rsémarrer les paramètres par défaut</translation>
+      <translation>Redémarrer les paramètres par défaut</translation>
     </message>
   </context>
   <context>
