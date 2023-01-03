bfd372e8906d3f8a62387d2e5cbad15f44dc39bf
Fabio Cavallo (FHorse) <punes.development@gmail.com>
2023-01-03 11:39:23 +0100

    New translations en_EN.ts (Italian)

--- src/gui/designer/translations/it_IT.ts
+++ src/gui/designer/translations/it_IT.ts
@@ -2519,6 +2519,10 @@
       <source>French</source>
       <translation>Francese</translation>
     </message>
+    <message>
+      <source>German</source>
+      <translation type="unfinished">German</translation>
+    </message>
   </context>
   <context>
     <name>wdgSettingsInput</name>
