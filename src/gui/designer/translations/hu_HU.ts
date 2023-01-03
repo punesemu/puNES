058146bd00e161b89aa140ba1ab7b18ee5f8b700
Fabio Cavallo (FHorse) <punes.development@gmail.com>
2023-01-03 13:24:26 +0100

    New translations en_EN.ts (Hungarian)

--- src/gui/designer/translations/hu_HU.ts
+++ src/gui/designer/translations/hu_HU.ts
@@ -2521,7 +2521,7 @@
     </message>
     <message>
       <source>German</source>
-      <translation type="unfinished">German</translation>
+      <translation>Német</translation>
     </message>
   </context>
   <context>
