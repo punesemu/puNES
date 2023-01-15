3d412c50a907f930c167a655a9012a35d5cdf336
Fabio Cavallo <punes.development@gmail.com>
2023-01-15 18:10:27 +0100

    I only handle spontaneous events (system events).
    Under Windows 7 64bit, pressing on one of the menu bar items and moving quickly with the mouse generates a mouseDoubleClickEvent event causing the GUI to be hidden/displayed when it is not actually required.

--- src/gui/wdgMenuBar.cpp
+++ src/gui/wdgMenuBar.cpp
@@ -25,7 +25,7 @@ wdgMenuBar::wdgMenuBar(QWidget *parent) : QMenuBar(parent) {}
 wdgMenuBar::~wdgMenuBar() = default;
 
 void wdgMenuBar::mouseDoubleClickEvent(QMouseEvent *event) {
-	if ((cfg->fullscreen == FULLSCR) && (gfx.type_of_fscreen_in_use != FULLSCR_IN_WINDOW)) {
+	if (!event->spontaneous() || ((cfg->fullscreen == FULLSCR) && (gfx.type_of_fscreen_in_use != FULLSCR_IN_WINDOW))) {
 		return;
 	}
 
