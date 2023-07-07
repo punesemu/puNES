1bdda82255efc47dc6f66e4b403a25634e735a89
Fabio Cavallo (FHorse) <punes.development@gmail.com>
2023-07-07 23:50:53 +0200

    New translations en_EN.ts (Polish)

--- src/gui/designer/translations/pl_PL.ts
+++ src/gui/designer/translations/pl_PL.ts
@@ -42,19 +42,19 @@
     <name>dlgDipswitch</name>
     <message>
       <source>Ok</source>
-      <translation type="unfinished">Ok</translation>
+      <translation>Ok</translation>
     </message>
     <message>
       <source>Dipswitch</source>
-      <translation type="unfinished">Dipswitch</translation>
+      <translation>Dipswitch</translation>
     </message>
     <message>
       <source>Start</source>
-      <translation type="unfinished">Start</translation>
+      <translation>Start</translation>
     </message>
     <message>
       <source>Default</source>
-      <translation type="unfinished">Default</translation>
+      <translation>Domyślne</translation>
     </message>
   </context>
   <context>
@@ -1517,7 +1517,7 @@
     </message>
     <message>
       <source>Dipswitch</source>
-      <translation type="unfinished">Dipswitch</translation>
+      <translation>Dipswitch</translation>
     </message>
   </context>
   <context>
@@ -1749,7 +1749,7 @@
     <message>
       <source>[red]error[normal] on write save state</source>
       <extracomment>Do not translate the words contained between parentheses (example: [red] or [normal]) are tags that have a specific meaning and do not traslate %1 and %2</extracomment>
-      <translation type="unfinished">[red]error[normal] on write save state</translation>
+      <translation>[red]błąd[normal] przy zapisywaniu stanu gry</translation>
     </message>
   </context>
   <context>
@@ -2573,7 +2573,7 @@
     </message>
     <message>
       <source>Polish</source>
-      <translation type="unfinished">Polish</translation>
+      <translation>Polski</translation>
     </message>
   </context>
   <context>
