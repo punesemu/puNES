93c81074900bfd67ea6c64f34965bf0f197d6a01
Fabio Cavallo (FHorse) <punes.development@gmail.com>
2024-01-07 14:01:56 +0100

    New translations en_en.ts (Italian)

--- src/gui/designer/translations/it_IT.ts
+++ src/gui/designer/translations/it_IT.ts
@@ -12,15 +12,15 @@
     <name>dlgAbout</name>
     <message>
       <source>portable version</source>
-      <translation type="unfinished">portable version</translation>
+      <translation>versione portatile</translation>
     </message>
     <message>
       <source>Nintendo Entertainment System Emulator</source>
-      <translation type="unfinished">Nintendo Entertainment System Emulator</translation>
+      <translation>Emulatore di Nintendo Entertainment System</translation>
     </message>
     <message>
       <source>Compiled</source>
-      <translation type="unfinished">Compiled</translation>
+      <translation>Compilato il</translation>
     </message>
   </context>
   <context>
