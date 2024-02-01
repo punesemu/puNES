bf1fed31ef76f6d85550f77a51fbadb294cebaad
Fabio Cavallo (FHorse) <punes.development@gmail.com>
2024-02-01 23:06:52 +0100

    New translations en_en.ts (Arabic (General))

--- src/gui/designer/translations/ar_AR.ts
+++ src/gui/designer/translations/ar_AR.ts
@@ -3146,10 +3146,6 @@
       <translation>كائنات لامحدودة</translation>
     </message>
     <message>
-      <source>Enable Extra Scanlines</source>
-      <translation>تفعيل خطوط مسح إضافية</translation>
-    </message>
-    <message>
       <source>Hide Sprites</source>
       <translation>إخفاء الكائنات</translation>
     </message>
@@ -3180,6 +3176,26 @@
     <message>
       <source>Automatically restores the limit when necessary to prevent graphical glitches</source>
       <translation>استعادة المحدودية عند الضرورة لتفادي تشوهات بصرية.</translation>
+    </message>
+    <message>
+      <source>Per-Game</source>
+      <translation type="unfinished">Per-Game</translation>
+    </message>
+    <message>
+      <source>Per-Game Overclock Setting</source>
+      <translation type="unfinished">Per-Game Overclock Setting</translation>
+    </message>
+    <message>
+      <source>On</source>
+      <translation type="unfinished">On</translation>
+    </message>
+    <message>
+      <source>Off</source>
+      <translation type="unfinished">Off</translation>
+    </message>
+    <message>
+      <source>Default Value</source>
+      <translation type="unfinished">Default Value</translation>
     </message>
   </context>
   <context>
