8f1a82dd3c4ba0ee83fa487032e8cc2063c4a45c
Fabio Cavallo <punes.development@gmail.com>
2024-02-16 00:38:48 +0100

    [SingleApplication] Fixed shared memory release issue with QT versions 6.6.0 or higher

--- src/extra/singleapplication/singleapplication.cpp
+++ src/extra/singleapplication/singleapplication.cpp
@@ -68,7 +68,7 @@ SingleApplication::SingleApplication( int &argc, char *argv[], bool allowSeconda
     // By explicitly attaching it and then deleting it we make sure that the
     // memory is deleted even after the process has crashed on Unix.
 #if QT_VERSION >= QT_VERSION_CHECK(6, 6, 0)
-    d->memory = new QSharedMemory( QNativeIpcKey( d->blockServerName ) );
+    d->memory = new QSharedMemory( QSharedMemory::legacyNativeKey( d->blockServerName ) );
 #else
     d->memory = new QSharedMemory( d->blockServerName );
 #endif
@@ -77,7 +77,7 @@ SingleApplication::SingleApplication( int &argc, char *argv[], bool allowSeconda
 #endif
     // Guarantee thread safe behaviour with a shared memory block.
 #if QT_VERSION >= QT_VERSION_CHECK(6, 6, 0)
-    d->memory = new QSharedMemory( QNativeIpcKey( d->blockServerName ) );
+    d->memory = new QSharedMemory( QSharedMemory::legacyNativeKey( d->blockServerName ) );
 #else
     d->memory = new QSharedMemory( d->blockServerName );
 #endif
