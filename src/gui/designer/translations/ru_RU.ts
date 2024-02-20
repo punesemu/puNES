a85231253d728dccbcaa7287b33307925dfa4071
Fabio Cavallo (FHorse) <punes.development@gmail.com>
2024-02-20 09:53:12 +0100

    New translations en_en.ts (Russian)

--- src/gui/designer/translations/ru_RU.ts
+++ src/gui/designer/translations/ru_RU.ts
@@ -84,7 +84,7 @@
     </message>
     <message>
       <source>Dip Switches</source>
-      <translation type="unfinished">Dip Switches</translation>
+      <translation>DIP-переключатель</translation>
     </message>
   </context>
   <context>
@@ -1683,7 +1683,7 @@
     </message>
     <message>
       <source>&amp;Dip Switches</source>
-      <translation type="unfinished">&amp;Dip Switches</translation>
+      <translation>&amp;DIP-переключатель</translation>
     </message>
   </context>
   <context>
