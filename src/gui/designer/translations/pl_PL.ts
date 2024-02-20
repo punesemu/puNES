c6686e500f11466005f1e3eff4a6794b2c5269f6
Fabio Cavallo (FHorse) <punes.development@gmail.com>
2024-02-20 09:35:24 +0100

    New translations en_en.ts (Polish)

--- src/gui/designer/translations/pl_PL.ts
+++ src/gui/designer/translations/pl_PL.ts
@@ -75,16 +75,16 @@
       <translation>Ok</translation>
     </message>
     <message>
-      <source>Dipswitch</source>
-      <translation>Dipswitch</translation>
-    </message>
-    <message>
       <source>Start</source>
       <translation>Start</translation>
     </message>
     <message>
       <source>Default</source>
       <translation>Domyślne</translation>
+    </message>
+    <message>
+      <source>Dip Switches</source>
+      <translation type="unfinished">Dip Switches</translation>
     </message>
   </context>
   <context>
@@ -1614,10 +1614,6 @@
       <translation>T&amp;aśma</translation>
     </message>
     <message>
-      <source>&amp;Dipswitch</source>
-      <translation>&amp;Dipswitch</translation>
-    </message>
-    <message>
       <source>Detach &amp;Barcode</source>
       <translation type="unfinished">Detach &amp;Barcode</translation>
     </message>
@@ -1683,7 +1679,11 @@
     </message>
     <message>
       <source>Open Floppy Disk Image</source>
-      <translation type="unfinished">Open Floppy Disk Image</translation>
+      <translation>Wczytaj obraz dyskietki</translation>
+    </message>
+    <message>
+      <source>&amp;Dip Switches</source>
+      <translation type="unfinished">&amp;Dip Switches</translation>
     </message>
   </context>
   <context>
